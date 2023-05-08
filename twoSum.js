3a10c0d61b3ef49181ea08820048819b02171489
Mai Vang <vmaineng@gmail.com>
2023-05-08 09:39:59 -0700

    added optimized solution

--- twoSum.js
+++ twoSum.js
@@ -1,5 +1,37 @@
-function twoSum(array) {
+function twoSum(array, target) {
+//take in an array, target
+//return the target indices
+//[0, 1 , 2, 3], 3 => 1, 2 b/c of the first incident
+
+//iterate through the array
+//add each index to the next index
+//check if sum of the two index are equaled to the target
+//return the indices located at the value
+
+// for (let i = 0; i < array.length; i++) {
+//     for (let j = i + 1; j < array.length; j++) {
+//         if (array[i] + array[j] === target) {
+//             // console.log(i, j)
+//             return [i,j]
+//         }
+//     }
+// }
+
+//optimized solution:
+
+let left = 0;
+let right = array.length - 1
+
+while (left < right) {
+    if (array[left] + array[right] === target) {
+        return [left, right]
+    } else if (array[left] < target) {
+        left++
+    } else {
+        right --
+    }
+}
 
 }
 
-console.log(twoSum([1,2,3,4]))
+console.log(twoSum([3,2,4], 6))
