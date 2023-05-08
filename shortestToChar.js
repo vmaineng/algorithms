ea40e37d841363359e699eb152f1a44e959879ba
Mai Vang <vmaineng@gmail.com>
2023-05-08 11:12:33 -0700

    notes for shortestToChar

---
+++ shortestToChar.js
@@ -0,0 +1,25 @@
+//leetcode 821
+
+const shortest = (s,c) => {
+    //create a new array caled answer
+    //take a word to see if the letter c exists in word
+    //return 0 if the letter is found, and the letters around it is taking i - j
+
+    //iterate through the s word 
+    //check to see if the first letter does not match c letter, 
+
+    //return answer;
+
+    let answer = [];
+    let difference = 0;
+
+    for (let i = 0; i< s.length;i++) {
+        if (s[i] !== c) {
+            let difference = Math.min(i -j)
+        }
+    }
+    return answer
+
+}
+
+console.log("loveleetcode", "e")
