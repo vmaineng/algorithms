fdf125358548a267b152517fcded0639ff1dd5d6
Mai Vang <vmaineng@gmail.com>
2023-05-11 10:23:57 -0700

    set up

---
+++ validPalindrome.js
@@ -0,0 +1,11 @@
+//leetcode 125
+
+const isPalindrome = (s) => {
+    //lowercase the string
+    //remove the empty spaces
+    //create left pointer at the start
+    //create right pointer at the end
+    //check if they equal each other
+    //if they do, keep traversing
+    //if they don't, return false
+}
