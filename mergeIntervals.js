a856471753246c3499eb2441f1440bef7cfb98a6
Mai Vang <vmaineng@gmail.com>
2023-05-16 13:02:33 -0700

    insert

---
+++ mergeIntervals.js
@@ -0,0 +1,10 @@
+const merge = (intervals) => {
+    
+//sort the index array by starting index
+//then compare end of first interval to beginning of second interval start
+//add it to the output
+
+
+}
+
+console.log(merge([[1,3],[2,6],[8,10],[15,18]]))
--- overlapIntervals.js
+++
@@ -1,5 +0,0 @@
-const merge = (intervals) => {
-
-}
-
-console.log(merge([[1,3],[2,6],[8,10],[15,18]]))
