67f5bbe856473916daf5dafaae53d889634b2e83
Mai Vang <vmaineng@gmail.com>
2023-05-16 16:33:22 -0700

    added in notes

--- mergeIntervals.js
+++ mergeIntervals.js
@@ -2,7 +2,21 @@ const merge = (intervals) => {
     
 //sort the index array by starting index
 //then compare end of first interval to beginning of second interval start
-//add it to the output
+//add it to the output is <= the second interval, then merge it in
+
+
+let newInterval = []
+
+//sort by first index, in-place
+intervals.sort((a,b) => a[0] - b[0])
+
+let temp = intervals[0]
+for (let i = 0; i < intervals.length; i++) {
+    if ( intervals[i][0] <= temp[1]) {
+        console.log( intervals[i][0], temp[1])
+
+    }
+}
 
 
 }
