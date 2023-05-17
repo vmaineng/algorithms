561a6eaa35a52e8687e477a82ebcd0131129c289
Mai Vang <vmaineng@gmail.com>
2023-05-16 18:32:30 -0700

    notes

---
+++ insertInterval.js
@@ -0,0 +1,19 @@
+const insert = (intervals, newInterval) => {
+
+
+//all the intervals before your mergin
+
+//after youre merging
+
+//all the intervals you need to merge
+
+
+}
+
+console.log(insert([[1,2],[3,5],[6,7],[8,10],[12,16]], [4,8]))
+
+//do things iteratively - ex: [3,5] - handle it first
+//push it ; then [3,5] => [3, 8]
+
+//look at start (if it's less than or equal to the end of the new interval) = merge
+
