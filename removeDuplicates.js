e4e60c6f1596a1d3037c8a43e7945fb3d1c19d25
Mai Vang <vmaineng@gmail.com>
2023-05-18 11:51:52 -0700

    remove algo done

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -1,27 +1,23 @@
 const removeDuplicates = (nums) => {
-    //input: array
-    //output: how many numbers are unique; integer
+  //input: array
+  //output: how many numbers are unique; integer
 
-    //brute force - for nested loops of i and j to keep track of each number
-    //next to each other
-    //keep track of how many unique numbers I find
+  //brute force - for nested loops of i and j to keep track of each number
+  //next to each other
+  //keep track of how many unique numbers I find
 
-    let i = 0;
+  let i = 0;
   for (let j = 1; j < nums.length; j++) {
     if (nums[i] !== nums[j]) {
-
       i++;
       //move i pointer
       nums[i] = nums[j];
       //reassign the value of where i pointer is the the value of j's poitner
-
     }
   }
-  console.log(i)
+  console.log(i);
   //i is pointing to previous value and needs to finish iterating through rest of array
   return i + 1;
+};
 
-
-}
-
-console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
+console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
