b81678c02379d62108de5c378e73a5823c22249a
Mai Vang <vmaineng@gmail.com>
2023-05-19 09:36:40 -0700

    corrected loops

--- reverseVowels.js
+++ reverseVowels.js
@@ -1,40 +1,33 @@
 const reverseVowels = (s) => {
-    //split out letters in strings
-    //create pointers
-    //iterate and create a temp variable
+  //split out letters in strings
+  //create pointers
+  //iterate and create a temp variable
+  //if found, switch them
+  //if the letter is not in the vowels string, keep iterating
 
-    //if the letter is not in the vowels string, keep iterating
-    //if found, switch them
-    //return the string
+  //return the string
 
+  let str = s.split("");
+  const vowels = "aeiou";
+  let i = 0;
+  let j = s.length - 1;
 
-const vowels = "aeiou"
-let str = s.split("")
-let i = 0; 
-let j = s.length - 1
-let temp = 0;
-
-while (i < j) {
-    if (!vowels.includes(str[i]) && !vowels.includes(str[j])) {
-       
-        j--
-        i++
-    } else if (vowels.includes(s[i])) {
-        temp = str[i]
-        str[i] = str[j]
-        str[j] = temp
-        i++
-    } else if (vowels.includes(s[j])) {
-        temp = str[i]
-        str[i] = str[j]
-        str[j] = temp
-        j++
+  while (i < j) {
+    if (vowels.includes(str[i]) && vowels.includes(str[j])) {
+      console.log(str[i], str[j]);
+      [str[i], str[j]] = [str[j], str[i] ]
+      i++;
+      j--;
     }
- 
-    return str.join("")
-}
+     if (!vowels.includes(s[i])) {
+        i++
+    } 
+    if(!vowels.includes(str[j])) {
+       j--
+    }
+  }
 
+  return str.join("");
+};
 
-}
-
-console.log(reverseVowels("leetcode"))
+console.log(reverseVowels("leetcode"));
