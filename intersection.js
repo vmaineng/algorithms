a2f257af01ee5090f224f106bf475dd80f0c9bdc
Mai Vang <vmaineng@gmail.com>
2023-05-22 09:47:37 -0700

    palidromeLL added

--- intersection.js
+++ intersection.js
@@ -2,15 +2,26 @@
 
 const intersection  = (nums1, nums2) => {
 //create start pointer for each index in the array
+//initialize an array to keep track of same values
 //check if they are the same,
 //then return the value they intersect at
 //tell it to keep going
 
+//can't use let i = 0 by itself; need i to iterate through nums1 array
+//can't use  if (nums1[i] === nums2[j]) {
+    //     return [nums1[i], nums2[j]]
+    // }
+//b/c need to keep track of unique values
+
+let sameValuesArray = []
 
 for (let i = 0; i < nums1.length; i++ ) {
 for (let j = 0; j < nums2.length; j++) {
     if (nums1[i] === nums2[j]) {
-        return [nums1[i], nums2[j]]
+       if (!sameValuesArray.includes(nums1[i])) {
+        i++;
+        j++;
+       } else if ()
     }
 }
 }
---
+++ palindromeLL.js
@@ -0,0 +1,22 @@
+const palidromeLL = (head) => {
+    //palindrome = same as it is forward as it is backward
+
+    //reverse LL 
+    //create pointers to point to prev node
+    //iterate through while current node does not equal null
+    //if the start equal the same as the last node,
+    //return true
+    //else it's false
+
+    if (!head) return [];
+
+    let current = head;
+    let prev;
+
+    while (current !== null) {
+
+    }
+
+}
+
+console.log(palidromeLL([1,2]))
