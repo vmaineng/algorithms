264136af99d5d2545f966de1627968f8e8d60882
Mai Vang <vmaineng@gmail.com>
2023-05-22 09:50:28 -0700

    brute force

--- palindromeLL.js
+++ palindromeLL.js
@@ -1,6 +1,7 @@
 const palidromeLL = (head) => {
     //palindrome = same as it is forward as it is backward
 
+    //brute force method
     //reverse LL 
     //create pointers to point to prev node
     //iterate through while current node does not equal null
@@ -8,15 +9,21 @@ const palidromeLL = (head) => {
     //return true
     //else it's false
 
-    if (!head) return [];
+    // if (!head) return [];
 
-    let current = head;
-    let prev;
+    // let current = head;
+    // let prev;
 
-    while (current !== null) {
+    // while (current !== null) {
+    //     let current = current.next
+    //     let prev = current
 
-    }
+    // }
+    // return true
 
+
+
+    
 }
 
 console.log(palidromeLL([1,2]))
