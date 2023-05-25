1f8624a69276befb6557f99cf09ef5a8305ead3a
Mai Vang <vmaineng@gmail.com>
2023-05-25 11:52:10 -0700

    added in solutions

--- longestPali.js
+++ longestPali.js
@@ -7,6 +7,26 @@ const longestPalindrome = (s) => {
     //store the results in a new string (+=)
     //return new string
 
+    //optimized method: 
+    //create two pointers (one at beginning, one at end)
+    //check if they are the same
+    //do one more check of the letters on the inside
+    //then add to the result
+
+    let left = 0;
+    let right = s.length - 1;
+
+    // ! when do we use split to get each letter
+
+    let letters = s.split("")
+    let longest = ""
+
+    while (left < right) {
+        if (letters[left] === letters[right]) {
+            longest += letters[left], letters[right]
+        }
+    }
+return longest
 
 }
 
