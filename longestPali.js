04be83f74ce58cde7ffe4ece9beccca1f8bce53d
Mai Vang <vmaineng@gmail.com>
2023-05-25 11:44:42 -0700

    longestPalidrome added

---
+++ longestPali.js
@@ -0,0 +1,13 @@
+const longestPalindrome = (s) => {
+
+    //substring = continous next to each other ; 
+    //palindrome = same as it is forward as it is backward
+
+    //brute force; two for nested loops and create poitners with i and j
+    //store the results in a new string (+=)
+    //return new string
+
+
+}
+
+console.log(longestPalindrome("cbbd"))
