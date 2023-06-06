3e8257e099197beab5393b19b6de2774e508f90c
Mai Vang <vmaineng@gmail.com>
2023-06-06 14:57:26 -0700

    added in pseudocode

---
+++ maxConsecutive.js
@@ -0,0 +1,12 @@
+const maxConsecutive = (nums) => [
+    //brute force: 
+    //create a current to keep track of the max:
+    //check the value at each index
+    //if they are the same, store them in an object
+    //then get the max out of current one that is stored vs the current one right now
+    
+
+    //optimized method: 
+    //create a current to keep track of the max:
+
+]
