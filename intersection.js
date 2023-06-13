bffad9c8bbe8ce55fdabccb2303489ea7d94f50a
Mai Vang <vmaineng@gmail.com>
2023-06-13 09:57:07 -0700

    worked on intersection

--- intersection.js
+++ intersection.js
@@ -1,6 +1,12 @@
 //leetcode 349
 
 const intersection  = (nums1, nums2) => {
+//brute force would to have one pointer in the first array at first index
+//and look at all the possibilities in second array to see if it matches the value
+//time = O(N^2) ? look at each two arrays
+//space = O(n) = storing a new array of intersection
+
+//optimized method:
 //create start pointer for each index in the array
 //initialize an array to keep track of same values
 //check if they are the same,
@@ -19,13 +25,12 @@ for (let i = 0; i < nums1.length; i++ ) {
 for (let j = 0; j < nums2.length; j++) {
     if (nums1[i] === nums2[j]) {
        if (!sameValuesArray.includes(nums1[i])) {
-        i++;
-        j++;
-       } else if ()
+       sameValuesArray.push(nums1[i])
+       }
     }
 }
 }
-
+return sameValuesArray
 }
 
 console.log(intersection([4,9,5], [9,4,9,8,4]))
