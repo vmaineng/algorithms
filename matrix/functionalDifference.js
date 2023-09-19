51ba73970692376b940be793e0e4b4264e8d7e5e
Mai Vang <vmaineng@gmail.com>
2023-09-18 17:58:58 -0700

    added in notes

--- matrix/functionalDifference.js
+++ matrix/functionalDifference.js
@@ -21,11 +21,16 @@ function diagonalDifference(arr) {
     let sum1 = 0;
     let sum2 = 0; 
     for(let i =0; i < arr.length; i++){
+        console.log(arr[i], arr[i][i]) //arr[i][i] is the row and column
         sum1 += arr[i][i];
-        sum2 += arr[i][arr.length - 1 - i];
+        sum2 += arr[i][arr.length - 1 - i]; // in reverse
+        
     }
     const finalDifference = Math.abs(sum2 - sum1)
     return finalDifference;
-    
 
 }
+
+let arr1 = [[11,2,4],[4,5,6],[10,8,-12]]
+
+console.log(diagonalDifference(arr1))
