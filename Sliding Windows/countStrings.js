067cb58f9f9d06e8d76dcaaff28ca173d5a89b20
Mai Vang <vmaineng@gmail.com>
2023-10-01 15:47:26 -0700

    added in string

--- Sliding Windows/countStrings.js
+++ Sliding Windows/countStrings.js
@@ -7,3 +7,33 @@
 // Note that if there are multiple occurrences of the same substring, every occurrence should be counted.
 
 // A substring is a contiguous sequence of characters in a string.
+
+
+// Input: s = "xyzzaz"
+// Output: 1
+// Explanation: There are 4 substrings of 
+// size 3: "xyz", "yzz", "zza", and "zaz". 
+// The only good substring of length 3 is "xyz".
+
+const countSubstring = (string) => {
+//receive a string in the parameter
+//return the number of good substrings in length of 3
+//"rawr" => "raw", "awr" => 2
+
+//brute force
+//check every possible substrings in length of 3
+
+//intiialize a window to 0; 
+//iterate through
+//check to see if length is 3; 
+
+//check in set to see if substring is unique = 1 good one
+
+let result = 0; 
+let k = 3;
+
+for (let i = 0; i < k; i++) {
+    
+}
+
+}
