3c4f945f093d903b9aaa4cee104b63d5c6f07a12
Mai Vang <vmaineng@gmail.com>
2023-10-17 14:24:26 -0700

    added in fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,12 @@
+Given a number as an input, print out every integer from 1 to that number. 
+However, when the integer is divisible by 3, print out “Fizz”; 
+when it’s divisible by 5, print out “Buzz”; 
+when it’s divisible by both 3 and 5, print out “Fizz Buzz”.
+
+
+
+const fizz = (number) => {
+    //get number as an input
+    //return if # is / 3 = "fizz", # is / 5 = "buzz", # is / 3 & / 5 = "fizz buzz"
+    //fizz(35) => "buzz", divisible by 5
+}
