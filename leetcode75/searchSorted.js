a563308735f8e2dcb51d5c25817b42fcc61d04f5
Mai Vang <vmaineng@gmail.com>
2023-10-31 09:52:24 -0500

    added in notes

--- leetcode75/searchSorted.js
+++ leetcode75/searchSorted.js
@@ -19,3 +19,43 @@
 
 // Input: nums = [4,5,6,7,0,1,2], target = 0
 // Output: 4
+
+//brute force - check if each position is our target = O(n) time
+
+
+//O (log n) time - binary search
+
+const searchSorted = (nums, target) => {
+//get an array of integers in ascending order & unique (which could be rotated), target = integer
+//return the index or -1 if can't find it
+//[-4, -3, 0, 4, 5, -5], 4 => 3 = index of 4
+
+//initialize a left pointer at beginning
+//initialize a right pointer at end
+//iterate through to make sure left and right pointer do not cross
+//check to see if middle is the target,
+//if so, return the index
+//if not middle index, if the target is smaller than middle index,
+//move right pointer
+//else move left pointer
+//return -1 if can't find index
+
+let left = 0;
+let right = nums.length - 1; 
+
+while (left < right) {
+    let middle = Math.floor ((left + right) / 2)
+
+    if (nums[middle] === target){
+        return middle
+    } else if (nums[middle] > target) {
+        left = middle + 1
+    } else {
+        right =middle - 1
+    }
+}
+return - 1;
+
+}
+
+console.log(searchSorted([-4, -3, 0, 4, 5, -5]))
