c0ee1b353cf7391119a26ae927e302a7ecee3d0b
Mai Vang <vmaineng@gmail.com>
2023-11-06 14:04:26 -0600

    added in inverttree

--- leetcode75/invertTree.js
+++ leetcode75/invertTree.js
@@ -14,4 +14,23 @@ const invertTree = (root) => {
 // 9       2
 
 
+//if tree root is empty, return null; 
+//recusively pass in the nodes
+//tell left node to replace right node
+//return the root
+
+if (!root) return root;
+
+invertTree(root.left);
+invertTree(root.right);
+
+let curr = root.left;
+root.left = root.right;
+root.right = curr;
+
+return root
+
+
 }
+
+console.log(invertTree([4,5,3,9,2]))
