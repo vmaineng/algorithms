3117b7bdf2ce1bd745ba57fda54c8679999cf46c
Mai Vang <vmaineng@gmail.com>
2023-11-06 14:56:07 -0600

    add in dfs and bfs

--- leetcode75/invertTree.js
+++ leetcode75/invertTree.js
@@ -21,12 +21,14 @@ const invertTree = (root) => {
 
 if (!root) return root;
 
-invertTree(root.left);
-invertTree(root.right);
-
+//swap nodes
 let curr = root.left;
 root.left = root.right;
 root.right = curr;
+
+invertTree(root.left);
+invertTree(root.right);
+
 
 return root
 
---
+++ leetcode75/maxDepth.js
@@ -0,0 +1,22 @@
+
+// Given the root of a binary tree, return its maximum depth.
+
+// A binary tree's maximum depth is the number of 
+// nodes along the longest path from the root node down to 
+// the farthest leaf node.
+
+var maxDepth = function(root) {
+//base case - 
+if (!root) return null;
+
+    //dfs - recrusive
+
+
+
+    //bfs - iterative
+
+
+
+};
+
+//doing recursion on the left subtrees and the right subtrees
