28e3b6b1e5dfc87f628c7b6e1601c59b92efaa7b
Mai Vang <vmaineng@gmail.com>
2023-11-14 10:52:41 -0600

    added in recursiveSearch.js

--- day1/countRecursive.js
+++ day1/countRecursive.js
@@ -18,3 +18,20 @@ function count(num) {
    
 }
 console.log(count(3))
+
+function recursiveSearch(arr, target) {
+//an array of integers, and an integer (whole numbers)
+//return the index found
+//[-2, 3, 4, 5], 3 => 1
+
+//iterate through the array
+//if array value === target, end
+//otherwise, keep iterating
+
+if (arr[0] === target) return "done";
+if (arr.length === 0) return "empty";
+
+return recursiveSearch(arr.slice(1), target);
+}
+
+console.log(recursiveSearch([1,2,3,4], 4))
