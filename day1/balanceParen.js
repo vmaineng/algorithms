df176548e9fce8c2ffd280f21bfc8e2dd2da7ac9
Mai Vang <vmaineng@gmail.com>
2023-11-15 09:34:57 -0600

    added balanceParen

---
+++ day1/balanceParen.js
@@ -0,0 +1,23 @@
+// For parentheses to be considered balanced, there must an opening parenthesis
+//  followed by a matching closing parenthesis. Given a string containing only 
+//  parentheses, return the number of additional parentheses needed for the 
+//  string to be considered balanced. 
+// The input string will have a minimum length of 1.
+
+const balanceParen = (string) => {
+    //string of open and closed parenthesis
+    //return the total amount of additional parenthesis needed
+    //'())' => 1
+    //'()())))((()))' => 3
+
+    //if string is less than 1, return 1
+    //initialize a count at 0;
+
+    //create an object of opening and closing
+    //iterate through the string and check in the object if there is a closing value
+    //if so, keep iterating
+    //otherwise, increment count to add more parenthesis
+    //return count;
+
+    
+}
