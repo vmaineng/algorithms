c5d22d1de67539fff660b7ab756968a1929a74a1
Mai Vang <vmaineng@gmail.com>
2023-11-16 10:24:15 -0600

    added in rotateArray

---
+++ day1/rotateArray.js
@@ -0,0 +1,12 @@
+// Given an input Array, rotate k units clockwise, i.e. 
+// shift the values rightward k units.
+
+// k is an Integer >= 0.
+
+function rotateArray(array, integer) {
+//receives an array of integers > 0, an integer stating how many rotations are made
+//return the new rotated array
+//[1,2,3,4], 7 => [4,1,2,3], [3,4,1,2], [2,3,4,1], [1,2,3,4], [4,3,2,1],[3,4,1,2], [2,3,4,1] => [2,3,4,1] 
+}
+
+console(rotateArray([1,2,3,4], 1))
