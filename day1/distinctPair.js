180de1bf59af9388bd11ff559c4d67b23d0d0d24
Mai Vang <vmaineng@gmail.com>
2023-11-20 12:34:19 -0600

    added in set solutions

--- day1/distinctPair.js
+++ day1/distinctPair.js
@@ -4,20 +4,50 @@
 // in each pair does not matter, e.g. we consider [[2, 8], [7, 3]] to be the
 //  same as [[3, 7], [8, 2]].
 
-function distinct(array, k) {
+function distinct(array, target) {
 //get an array of positive & whole integers, and a positive&whole integer
 //return back the pair values and ensure the pair values are different from the other pair
 //[9, 4, 5, ,-3, 5], 9 => [ 4, 5]
 
 //edge cases: if the array is less than 1, can't make a pair
 
-//initialize an array result to capture pairs
+//intialize a result array to capture pairs
 //iterate
 //see if it adds up to target
 //push into the result array
 //return it
 
+if (array.length < 2) return "pair cannot be found";
 
+// let result =[];
+
+// for (let i = 0; i < array.length; i++) {
+//     if (array[i] + array[i + 1] === target) {
+//         result.push([array[i], array[i + 1]]) //initializing array outside will help create separate arrays
+//     }
+// }
+// return result
+
+//optimized
+//use a set to keep track of unique pairs
+
+//iterate
+//ask if set has not seen pairs, add to the set
+
+let result = [];
+let seenSet = new Set();
+
+for (let i = 0; i < array.length - 1; i++) {
+    let currentNum = array[i];
+    let nextNum = array[i + 1];
+    let pair = [currentNum, nextNum]
+
+    if (currentNum + nextNum === target && !seenSet.has(pair.toString())) {
+        result.push(pair);
+        seenSet.add(pair.toString())
+    }
+}
+return result;
 
 }
 
