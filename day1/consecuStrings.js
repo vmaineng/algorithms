aacaa80ef4d39bddf0982d698dfd6f65ac7af577
Mai Vang <vmaineng@gmail.com>
2023-11-21 09:52:36 -0600

    added in n squared solutions

--- day1/consecuStrings.js
+++ day1/consecuStrings.js
@@ -15,8 +15,32 @@
 
 function consecutiveString(string) {
 //get a string's length of 0 or more
-//return all the consecutive substrings of at least one character
-//
+//return all the consecutive substrings of at least one character in an array
+//'green' => 'g', 'gr', 'gre', 're', 'en','n'
+
+//edge cases: if string is empty, return string
+//if string is one, retun the string 
+
+//initialize an empty result array
+//split the strings
+//iterate through the string
+//push the string in
+//return the result array
+
+// const substrings = [];
+
+// for (let i = 0 ;i < string.length; i++) {
+//   for (let j = i + 1; j <= string.length; j++ ) { // the inner loop captures the last element
+//     substrings.push(string.substring(i, j));
+//   }
+// }
+
+// return substrings;
+
+//time: O(n^2)
+//space: O(n) depends on how many characters in string
+
+
 }
 
 console.log(consecutiveString('abc'))
