093d5168cfeb6d329532e503a776020835e00466
Mai Vang <vmaineng@gmail.com>
2023-11-21 09:26:29 -0600

    added in consecutiveString

---
+++ day1/consecuStrings.js
@@ -0,0 +1,22 @@
+// Given a string, return all consecutive substrings within that string 
+// consisting of at least one character. Substrings should be returned in the 
+// order in which they appear.
+
+// Note than in the string 'abc', 'ac' is not a consecutive substring.
+
+// The input string will have a length of 0 or more.
+
+// Input: 'abc'
+// Output: ['a', 'ab', 'abc', 'b', 'bc', 'c']
+
+// Input: 'a'
+// Output: ['a']
+
+
+function consecutiveString(string) {
+//get a string's length of 0 or more
+//return all the consecutive substrings of at least one character
+//
+}
+
+console.log(consecutiveString('abc'))
