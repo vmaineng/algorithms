df0a6aad0405ab3dc022946cd1620cb5e10aa4a0
Mai Vang <vmaineng@gmail.com>
2023-12-04 10:25:40 -0800

    added in notes

--- redos/validPalind.js
+++ redos/validPalind.js
@@ -1,3 +1,6 @@
 const validPali = (string) => {
-
+//get a string of letters - uppercase and lowercase
+//return true if it is a palidrome, else false
+//palidrome = spelled the same forward as it is backwards
+//
 }
