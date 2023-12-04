188ea1810ff1cfac74d60f4cac04fc0a91588755
Mai Vang <vmaineng@gmail.com>
2023-12-04 10:52:33 -0800

    added in time and space

--- redos/validPalind.js
+++ redos/validPalind.js
@@ -2,5 +2,25 @@ const validPali = (string) => {
 //get a string of letters - uppercase and lowercase
 //return true if it is a palidrome, else false
 //palidrome = spelled the same forward as it is backwards
-//
+//"apple" => false; "batab" => "false"
+
+//create a new string starting from the ending letter
+//add to string
+//check if strings are the same, return true
+//else return false
+
+// if (string.length === 0) return false;
+
+// let reversedString = "";
+// for (let i = string.length - 1; i >= 0; i--) {
+//     reversedString += string[i]
+// }
+
+// return reversedString === string ? true : false
+
+//time = O(n) b/c going through the string once
+//space = O(n) b/c creating a new string as much as string's input intakes
+
 }
+
+console.log(validPali("appa"))
