6530c8562b569a848d78070203887d1104d5526e
Mai Vang <vmaineng@gmail.com>
2023-12-06 09:58:07 -0800

    added in longest consec

---
+++ redos/conse.js
@@ -0,0 +1,7 @@
+const longestConsecutive = (array) => {
+//get an array of numbers; could be positive or negative numbers
+//return the length of the longest consecutive sequence listed
+//[-4, 14, 12, -3, -5, 1, 0] => [-5, -4, -3]
+}
+
+console.log(longestConsecutive([100, 4, 100, 1, 3, 2,5]))
