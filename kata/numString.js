3e70e96cb8d623e4e98c0cd6c7ff8738decba833
Mai Vang <vmaineng@gmail.com>
2023-12-12 10:23:57 -0800

    added in reduce method

---
+++ kata/numString.js
@@ -0,0 +1,44 @@
+//convert number to a string
+
+function numberToString(num) {
+//an integer (whole)
+//return the integer to strings
+//345 => '345
+
+return num.toString();
+
+}
+
+//console.log(numberToString(456))
+
+function ops(num) {
+    //a whole integer
+    //return the opposite of number
+    //-4 => 4
+
+    return (-num)
+}
+// console.log(ops(-4))
+
+function positiveSum(arr) {
+//get an array of num both positive and negative
+//return total of positive numbers
+//[1, -4, 3, 5 ] => 9; [-3, -4, -5] => 0
+
+//initialize a total at 0;
+//iterate through the nums
+//check to see if greater than 0, then add to total
+//return total;
+
+// let total = 0;
+
+// for (const num of arr) {
+//     if (num > 0) {
+//         total += num
+//     }
+// }
+// return total; 
+return arr.reduce((a,b) => a + (b > 0 ? b : 0), 0)
+}
+
+console.log(positiveSum([1, -4, 3, 5]))
