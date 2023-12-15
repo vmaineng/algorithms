cf4ddb05833d7be8ce9f070e600db7b508f1809e
Mai Vang <vmaineng@gmail.com>
2023-12-15 09:43:41 -0800

    added in zipper list

--- struct/rrLLri.js
+++ struct/rrLLri.js
@@ -31,6 +31,55 @@ const reverseList = (head) => {
       const next = head.next;
       head.next = prev;
       return reverseList(next, head) //next will replace head in the paramenters
-      //head will replace prev=null
+      //head will replace prev
+      //return the new head of the linked list
     
     };
+
+    //time:O(n)
+    //space:O(n)
+
+
+    // class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+
+const zipperLists = (head1, head2) => {
+
+  
+  //two linked lists that are not empty
+    //return one linked list alternating nodes
+    //a->b->c, e-> f -> g => a->e->b->f->c->g
+    
+    //create a dummy node
+    //create a count - odd(take from first list) & even (then take second list)
+    //iterate through the linked list until it's null
+    //have dummy node point to first linked list
+    //then point to second list
+    //return dummy.next 
+    
+    let dummy = null;
+    let count = 0;
+    let current1 = head1;
+    let current2 = head2;
+    
+    while (current1 !== null && current2 !== null) {
+      if (count % 2 === 0){
+      dummy.next = current2
+        current2 = current2.next;
+  } else {
+     dummy.next =current1
+        current1 = current1.next;
+  }
+      dummy = dummy.next=current1    
+      count += 1;
+  }
+  
+  if (current1 !== null) tail.next = current1;
+    if (current2 !== null) tail.next = current2;
+  
+  return dummy.next;
+  };
