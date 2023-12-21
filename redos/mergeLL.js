56b3191a09c24418360f186233e81b63243540e8
Mai Vang <vmaineng@gmail.com>
2023-12-21 09:08:52 -0800

    added in mergeLL

---
+++ redos/mergeLL.js
@@ -0,0 +1,9 @@
+const mergeTwo = (link1, link2) => {
+    //get the head of two sorted linked lists
+    //return one linked lists with two lists combined in order
+    //1 -> 2 -> 6 -> 8, 3 -> 5 -> 10 -> 11 -> 12
+    // => 1 -> 2 ->3 -> 5 ->  -> 6 -> 8-> 10 -> 11 -> 12
+
+    //edge cases: if one of the linked lists is empty, return the other one
+    
+}
