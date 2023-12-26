83a6ca5d1161c45df15f1e6bdea9cf7ec5fed873
Mai Vang <vmaineng@gmail.com>
2023-12-26 12:25:05 -0800

    added in solutions

--- matrix/searchSorted.js
+++ matrix/searchSorted.js
@@ -4,7 +4,25 @@ const searchSorted = (matrix, target) => {
 
 
 //if matrix is empty, return null;
-//
+//find the row and the column
+//search until target has been found
+//if greater, move column to the left
+//move row down
+//return the row and column
+
+let row = 0; 
+let column = matrix[0].length - 1; //column at first row
+
+while (row < matrix.length && column >= 0) {
+    if (matrix[row][column] > target) { //move column to the left
+        column -= 1
+    } else if ( matrix[row][column] < target) {
+        row += 1 //move row down
+    } else {
+        return [row, column]
+    }
+}
+return [-1, -1]
 }
 
 console.log(searchSorted())
