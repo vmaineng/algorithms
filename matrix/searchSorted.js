888b541f717e67b73243a5aacbe8c70b30aad091
Mai Vang <vmaineng@gmail.com>
2023-12-26 12:19:48 -0800

    added in notes

---
+++ matrix/searchSorted.js
@@ -0,0 +1,10 @@
+const searchSorted = (matrix, target) => {
+//matrix, and a coordinate
+//return the location of the coordinates
+
+
+//if matrix is empty, return null;
+//
+}
+
+console.log(searchSorted())
