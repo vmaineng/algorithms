75919da610ff3e850066594df0a7ca6da91600ef
Mai Vang <vmaineng@gmail.com>
2024-01-01 13:57:13 -0800

    added in bubble

---
+++ sort/bubble.js
@@ -0,0 +1,14 @@
+const bubbleSort = (array) => {
+
+for (let i = 0 ;i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] > array[j]) {
+        let temp = array[i]
+        array[i] = array[j]
+        array[j] = temp;
+        
+      }
+    }
+  }
+  return array;
+}
