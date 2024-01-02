488a8f7f3fc5aa316e23b164898d0910560b247c
Mai Vang <vmaineng@gmail.com>
2024-01-02 10:05:43 -0800

    added in notes

--- sort/insertion.js
+++ sort/insertion.js
@@ -13,9 +13,15 @@ function insertionSort(array) {
     for (let i = 1;i  < array.length; i++) {
      let j = i;
       while (j > 0 && array[j] < array[j-1]) {
-        [array[j], array[j-1]] = [array[j-1],  array[j]]
-        j -= 1;
+        [array[j], array[j-1]] = [array[j-1],  array[j]] //swap the numbers
+        j -= 1; //decrement j b/c if an element has been pushed in front of what was the current
+        //j, compare the next value in line to the newest j
       }
     }
     return array;
   }
+
+  console.log(insertionSort([0, 4, -10, 23, 100, -52]))
+
+  //time: O(n^2)
+  //space: O(1)
