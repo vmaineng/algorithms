61ec764e0989a5d7faf28a8ebdfc67f8beea3af4
Mai Vang <vmaineng@gmail.com>
2024-01-02 11:04:07 -0800

    added in notes for selection

--- sort/selection.js
+++ sort/selection.js
@@ -12,22 +12,24 @@ function selectionSort(array) {
     //create another pointer to see if the number is less than the first number
     //if so, then push it to the end of the list
     //return the array
+
+    //create a pointer starting on teh first number
+    //loop til the number efore the end of the array b/c last number is assumed to be in the correct order
+    //have another pointer to find if there's another smaller number
+    //if so swap it
   
-    currentIdx = 0;
-  
-    while (currentIdx < array.length - 1) {
-      smallestIdx = currentIdx;
-      for (let i = currentIdx + 1; i < array.length; i++) {
-        if (array[smallestIdx] > array[i]) {
-          smallestIdx = i
-          swap(smallestIdx, currentIdx, array)
-        }
-        currentIdx++
-      }
+ for(let i =0; i < array.length - 1; i++) {
+    let smallestIdx = i;
+   for (let j = i + 1; j < array.length; j++) {
+    if (array[smallestIdx] > array[j]) {
+        smallestIdx = j
     }
+   }
+   [array[i], array[smallestIdx]] = [array[smallestIdx], array[i]]
+ }
     return array;
   }
   
-  function swap(i, j, array) {
-  [array[i], array[j]] = [array[j], array[i]]
-  }
+//   function swap(i, j, array) {
+//   [array[i], array[j]] = [array[j], array[i]]
+//   }
