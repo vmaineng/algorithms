4f1fbfadad9f0c7a1ac9b0692839801f386c6c1a
Mai Vang <vmaineng@gmail.com>
2024-01-04 11:31:38 -0800

    added in key

--- sort/quick.js
+++ sort/quick.js
@@ -2,4 +2,8 @@
 //move values smaller of the value to th left
 //move values bigger to the right
 //then the pivot value will naturally be in the correct sorted position
-//typically pick the first value in the array as the pivot value
+//typically start out with the first value in the array as the pivot value
+
+
+//!always swap left and right number when the left number is greater than pivot and right
+//!number is less than the pivot
