592710524665fd33b5494cdd79e5de5c3eee6813
Mai Vang <vmaineng@gmail.com>
2024-01-04 11:22:10 -0800

    added in pivot

--- sort/quick.js
+++ sort/quick.js
@@ -1,1 +1,5 @@
 //objective: pick a pivot value
+//move values smaller of the value to th left
+//move values bigger to the right
+//then the pivot value will naturally be in the correct sorted position
+//typically pick the first value in the array as the pivot value
