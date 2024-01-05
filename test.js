4a50aca4d1b15111041e9fecd4ac2b7d0685a434
Mai Vang <vmaineng@gmail.com>
2024-01-05 12:00:17 -0800

    updated reduceString

--- test.js
+++ test.js
@@ -26,11 +26,11 @@ return false;
 
 }
 
-console.log(containDuplicates([2,14,18,22,22]))
+// console.log(containDuplicates([2,14,18,22,22]))
 
 const reduceString = (string) => {
-    let wordSplit = string.split(" ")
-    return wordSplit.reduce((acc, cv) => acc + cv, " ")
+   
+    return string.split("").reduce((acc, cv) => cv + acc, "")
 }
 
 console.log(reduceString("the jack in the box"))
