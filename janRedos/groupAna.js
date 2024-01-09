bbcc39ebb99b3a508ff86d9de2273ac430f55be7
Mai Vang <vmaineng@gmail.com>
2024-01-09 09:28:58 -0800

    added in groupAna

--- janRedos/groupAna.js
+++ janRedos/groupAna.js
@@ -1,8 +1,31 @@
 const groupAna = (array) => {
     //an array of strings
     //return similar anagrams together
-    
+    //["gre", "reg", "erg", "app"] => [["app"], ["gre", "reg", "erg"]]
 
+    //if the array of string has only one string, return the string
+    //if array is empty, return an empty string
+
+    if (array.length === 1) return [array];
+    if (array.length === 0) return [""];
+
+    //create an array to capture the sorted results
+    //iterate through each word, sort them
+    //push them into the array
+    //return the array
+
+    let groupedAnagrams = {};
+
+
+    for (let word of array) {
+       let sortedWord = word.split("").sort().join("")
+        if (!groupedAnagrams[sortedWord]) {
+            groupedAnagrams[sortedWord] = [word];
+        } else {
+            groupedAnagrams[sortedWord].push(word)
+        }
+    }
+return Object.values(groupedAnagrams)
 }
 
 console.log(groupAna(["eat","tea","tan","ate","nat","bat"]))
