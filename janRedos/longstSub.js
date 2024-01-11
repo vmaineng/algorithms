0ea7da9b1a85bf958507cc595714fce22c1284e4
Mai Vang <vmaineng@gmail.com>
2024-01-11 10:44:49 -0800

    added in set

---
+++ janRedos/longstSub.js
@@ -0,0 +1,17 @@
+function longestSub(string) {
+    //get a string of letters
+    //return the longest substring w/ no repeating characters
+    //'abcdeba'=> 'abcde' => 5
+
+    //if string has only one letter, return 1
+
+    //create a set to hold unique characters
+    //create a pointer on the first letter and second letter
+    //keep moving the right letter down, add it to set
+    //if set already has the letter
+    //return set.size
+
+
+
+    
+}
