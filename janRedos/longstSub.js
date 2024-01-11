a70ca4d8ed2913da1576159e7f516fad76365102
Mai Vang <vmaineng@gmail.com>
2024-01-11 12:16:01 -0800

    added in longestSub

--- janRedos/longstSub.js
+++ janRedos/longstSub.js
@@ -6,12 +6,31 @@ function longestSub(string) {
     //if string has only one letter, return 1
 
     //create a set to hold unique characters
+    //store the max integer
     //create a pointer on the first letter and second letter
     //keep moving the right letter down, add it to set
     //if set already has the letter
     //return set.size
 
 
+const hasLetter = new Set();
+let i = 0;
+let max = 0;
 
-    
+for (let j = 0; j < string.length; j++) {
+   while (hasLetter.has(string[j])) {
+        hasLetter.delete(string[i])
+        i++;
+   }
+   hasLetter.add(string[j])
+   max = Math.max(max, hasLetter.size )
 }
+return max;
+}
+
+console.log(longestSub('abcdeba'))
+
+//objective: 
+//j accounts for new letter so need to check if the letter at j exists yet
+//seaerch the entire word
+//which then requires shrinking the previous (window) letter seen at i
