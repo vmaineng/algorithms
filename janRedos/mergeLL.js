9079cfc27fd2e91d6cf68847090ac7c94e6c9345
Mai Vang <vmaineng@gmail.com>
2024-01-12 10:43:17 -0800

    added in linked list

---
+++ janRedos/mergeLL.js
@@ -0,0 +1,20 @@
+const mergeLL = (head1, head2) => {
+    //get a head of two sorted linked list
+    //return one sorted linked list
+    //
+
+    //edge cases:
+    //if the one of the head is empty, return the other one
+
+    //keep count of odd and even
+    //if odd, merge the linked list from the first linked list
+    //if even, add in the node from second linked list
+
+    //if left overs from first linked list, add it in
+    //if left overs from second linked list, add it in
+
+    //return the head of linked list
+
+    
+
+}
