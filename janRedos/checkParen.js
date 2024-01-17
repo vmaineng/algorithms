c582dd43aea6c09ce7c1f665cfacbfcadea10ba4
Mai Vang <vmaineng@gmail.com>
2024-01-17 10:47:17 -0800

    added in pariings object

--- janRedos/checkParen.js
+++ janRedos/checkParen.js
@@ -2,6 +2,24 @@ const checkParen = (string) => {
 //get a string of paranethesis
 //return true if opening matches with closing
 //'{{[]}' => false => '{'
+
+//if string length is 1, return false
+
+//create an object: of matching opening and closing parenthesis
+
+//iterate through the string
+//check to see if the closing parenthesis matches
+//then pop off of the stack
+//return if the stack's length is equal to the string's length
+
+if (string.length === 1) return false;
+
+let pairings = {
+    '[': ']',
+    '{': '}',
+    '(': ')'
+}
+
 }
 
 console.log(checkParen('{{}'))
