e782586140b5cbd0d001d0daf4fecc7f236d38f1
Mai Vang <vmaineng@gmail.com>
2024-01-18 08:53:19 -0800

    added in replaceChar

--- janRedos/checkParen.js
+++ janRedos/checkParen.js
@@ -20,6 +20,35 @@ let pairings = {
     '(': ')'
 }
 
+let stack = [];
+
+for (let char of string){
+    if (pairings[char]) {
+        stack.push(pairings[char])
+    } else {
+        if (stack.pop() !== char) {
+            return false
+        }
+    }
+}
+return !stack.length;
+
 }
 
 console.log(checkParen('{{}'))
+
+function makeNew(array) {
+   let result = [];
+   for (let i = 0 ; i < array.length; i++) {
+   result.push([array[i], array[i + 1]])
+   }
+   return result;
+}
+
+console.log(makeNew([0,1,]))
+
+const replaceChar = (s,k) => {
+    //get a string of letter, and an integer that states how many letters can be replaced
+    //return the length of the longest substring containing same letters
+    //'ARGRRGR',4 => 'RRRRRRR' => 7
+}
