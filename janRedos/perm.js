2984b43a3e1b86b4c3c91aacd557a42dfa214e81
Mai Vang <vmaineng@gmail.com>
2024-01-23 12:08:20 -0800

    added in perm2

--- janRedos/perm.js
+++ janRedos/perm.js
@@ -16,18 +16,45 @@ const perm = (s1, s2) => {
 //once we have seen the same values, return true
 //else return false
 
-if (s2.length < s1.length) return false;
+if (s1.length > s2.length) return false;
 
-let seen = {};
+let seen1 = {}; //tracks frequency/count of string1
 
-let si1 = 0;
+for (let i = 0; i < s1.length; i++) {
+    seen1[s1[i]] = (seen1[s1[i]] || 0) + 1
+}
+console.log(seen1)
 
-for (let i = 0; i < s2.length; i++) {
+let left = 0;
+let right = 0;
+let s1Length = s1.length;
+
+while (right < s2.length) {
+  
+    if (seen1[s2[right]] > 0) s1Length--;
+    seen1[s2[right]]--;
+    right++
+
+    if (s1Length === 0) return true;
+
+    if (right - left === s1.length) {
+        if (s1[s2[left]] >= 0) s1Length++;
+        seen1[s2[left]]++
+        left++
+    }
+}
+return false;
 
 }
 
-}
-
-console.log(perm('ab', 'rhinabo'))
+console.log(perm('abdfs', 'rhinabo'))
 
 //objective; keep a fixed size window
+//keep track of frequencies to make sure they match
+//decrement from s1's length until its 0, return true;
+//else after searching entire string, return false 
+
+
+const perm2 = (s1, s2) => {
+    
+}
