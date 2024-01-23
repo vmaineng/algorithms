651832eba2ea25f62c40d1f81ae99559f4a2477c
Mai Vang <vmaineng@gmail.com>
2024-01-23 10:37:35 -0800

    added in perm

---
+++ janRedos/perm.js
@@ -0,0 +1,11 @@
+const perm = (s1, s2) => {
+//get two strings of a word
+//return true if s2 contains s1
+//'barbie', 'aquabarbuous' => false
+//'ba', 'barbie' => false
+
+
+
+}
+
+console.log(perm('ab', 'rhinabo'))
--- janRedos/selection.js
+++ janRedos/selection.js
@@ -3,6 +3,9 @@ const selection = (array) => {
     //return an array of sorted numbers
 
     //[34, 5, 2, 8, 9] => [2, 5, 8, 9, 34]
+    // i   j
+//smallestidx
+    //smallestidx      
 
     //create two sublists - first one is sorted, second is unsorted
 
@@ -13,11 +16,13 @@ const selection = (array) => {
         let smallestIdx = array[i]
         for (let j = i + 1; j < array.length; j++) {
             if (array[smallestIdx] > array[j]) {
-                smallestIdx = j
-              
+                smallestIdx = j 
             }
         }
-        [array[j] = array[j], smallestIdx
+        //smallestIdx is where j is located 
+        //swap j's value
+        [array[i], array[smallestIdx]] = [array[smallestIdx], array[i]] //! swap with value at i
     }
+    return array;
 
 }
