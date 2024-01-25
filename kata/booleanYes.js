f0edc4d53d336b500290cbc7d7fc45396bb095dc
Mai Vang <vmaineng@gmail.com>
2024-01-25 10:30:07 -0800

    added in sqroot

--- kata/booleanYes.js
+++ kata/booleanYes.js
@@ -28,9 +28,46 @@ return bool ? "Yes" : "No"
      //capture the substring and remove the first and last
      //return the string
      
-   for (let i = 1; i < str.length; i++) {
-     str.substring(i, str.length - 1)
-   }
-     return str;
+//    for (let i = 1; i < str.length; i++) {
+//     for (let letter of str) {
+//      let splitWord = str.substring(letter, str.length - 1)
+//      console.log(splitWord)
+//    }
+    //  return splitWord;
+
+    // return str.substring(1, str.length-1)
+    return str.slice(1, -1) //slice extracts a section of the string
    
    };
+
+//    console.log(removeChar('apple'))
+
+
+   function squareSum(numbers){
+    //an array of numbers
+      //return the sum of all numbers from array
+      
+      //[1, 2, 3] => 1^2 + 2^2 + 3^2 = 1 + 4 + 9 = 14
+      
+      //intiialize a sum
+      //iterate through the numbers
+      //square root it
+      //add to sum
+      //return sum
+      
+      let sum = 0; 
+      
+      for (let i of numbers) {
+    // for (let i = 0; i < numbers.length; i++){
+        const sqRoot = i**2
+        console.log(sqRoot, i)
+        sum += sqRoot
+      }
+
+// let sqRtNums = numbers.map(num => Math.exp(num))
+// console.log(sqRtNums)
+
+      return sum
+    }
+
+    console.log(squareSum([1,2,3]))
