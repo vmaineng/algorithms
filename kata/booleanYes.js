38f3c7a36376c4c1ba083e9777fb5e8ccb6b2f78
Mai Vang <vmaineng@gmail.com>
2024-01-25 10:11:28 -0800

    added in removeChar

--- kata/booleanYes.js
+++ kata/booleanYes.js
@@ -10,6 +10,27 @@ function boolToWord( bool ){
 //     return "No"
 //   }
   
-return bool === false ? "No" : "Yes"
+// return bool === false ? "No" : "Yes"
+
+return bool ? "Yes" : "No"
+
+//bool itself is a boolean value and its truthiness is being checked;
 
   }
+
+
+  function removeChar(str){
+    //get a string of word with no spaces and all lowercase
+     //return the string back with first and last letter not included
+     //'apple' => 'ppl'
+     
+     //iterate through the string
+     //capture the substring and remove the first and last
+     //return the string
+     
+   for (let i = 1; i < str.length; i++) {
+     str.substring(i, str.length - 1)
+   }
+     return str;
+   
+   };
