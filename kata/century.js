cf18b397330cb7dfbaf22d9ba2eada5c7655cd6d
Mai Vang <vmaineng@gmail.com>
2024-01-26 10:45:31 -0800

    added in abbrev

--- kata/century.js
+++ kata/century.js
@@ -1,7 +1,7 @@
 function century(year) {
     //receive integers for a year
     //return what century it is in
-    //'3000'=> 29
+    //'3000'=> 30
     
     //if stmts
     
@@ -14,5 +14,32 @@ function century(year) {
     
     //optimized solution: add one to the yearCount
     //century = 100 years
-    return Math.ceil (year/100)
+    return Math.ceil (year/100) //using ceiling method to round up to nearest century (100)
     }
+
+    // console.log(century(3000))
+
+    function abbrevName(name){
+
+        //receive a name with a space
+      //return initials uppercase
+      //"ma van" => "M.V"
+    
+      //edgecase:
+      //if they only provide first or last name, capitalize the first letter
+      
+    //create a new string
+      //iterate through
+      //push the first letter of first part
+      //push first letter of second part
+    //return string uppercase
+    
+//split on spaces
+//map through it and grab first letter
+//return it capitalize
+
+return name.split(" ").map(letter => letter[0].toUpperCase()).join(".")
+      
+    }
+
+    console.log(abbrevName("ma van"))
