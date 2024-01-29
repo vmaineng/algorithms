317c3fd434f17b2757e54376dc673aca8dbcf7cf
Mai Vang <vmaineng@gmail.com>
2024-01-29 10:32:49 -0800

    added in sheep

--- kata/stringRepeat.js
+++ kata/stringRepeat.js
@@ -17,3 +17,58 @@ function repeatStr (n, s) {
     
     return s.repeat(n)
   }
+
+
+  class SmallestIntegerFinder {
+    findSmallestInt(args) {
+
+
+      //get an array of integer
+      //return the smallest value
+      
+      //[-342,-453, 452,47, 8] => -453
+      
+      //set a minNumber to 0;
+      //iterate through looking to see if this should be the new min
+      //return the minNumber
+      
+      let minNumber = 0;
+      for (let i = 0; i < args.length; i++) {
+        console.log(array[i])
+        minNumber = Math.min(minNumber, array[i])
+        return minNumber
+      }
+
+
+      //return Math.min(...args)
+    }
+  }
+
+  console.log(SmallestIntegerFinder(findSmallestInt([-342,-453, 452,47, 8])))
+
+  function countSheeps(sheep) {
+    //an array of true or false
+    //return the sheeps present in the array
+    //[true, false, true, true] => 3
+    
+    //edge case:
+    //if null or undefined or empty, return 0 
+    
+    //keep count
+    //look through each element
+    //if true, add to count
+    //if false, ignore
+    
+    //return count
+    
+    if (!sheep || sheep === undefined || sheep === null) return 0;
+    
+    let count = 0;
+    for (let i = 0;i < sheep.length; i++) {
+      if (sheep[i] === true) {
+        count += 1
+      }
+    }
+    return count;
+  
+  }
