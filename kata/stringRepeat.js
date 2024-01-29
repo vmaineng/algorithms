1df4c7e8a5bf1c2f85a0329987fce1969a0e3094
Mai Vang <vmaineng@gmail.com>
2024-01-29 09:26:22 -0800

    added in stringRepeat

---
+++ kata/stringRepeat.js
@@ -0,0 +1,19 @@
+function repeatStr (n, s) {
+    //get an integer stating how many times to repeat, s is a string
+    //return a string of the repeated word/letter provided n times
+    
+    //ex: (10,"Mai") => "MaiMaiMaiMaiMaiMaiMaiMaiMaiMai"
+    
+    //create a new string
+    //add the s word/char on to the string
+    //if the times it's been added exceeds n, then stop
+    //return string
+    
+  //   let newString = "";
+  //   for (let word of s) {
+  //     newString += word
+  //     if ( n )
+  //   }
+    
+    return s.repeat(n)
+  }
