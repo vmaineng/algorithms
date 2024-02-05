047f04930bf017291eea23d56c60f0382f616cf1
Mai Vang <vmaineng@gmail.com>
2024-02-05 10:15:49 -0800

    added in group anagram

---
+++ febRedos/anagramGroup.js
@@ -0,0 +1,9 @@
+const anaGroup = (strs) => {
+//an array of strings
+//return anagrams grouped together
+//anagram = using all the letter in each word once you get another word
+
+//["cat", "bat", "float", "toalf"] => [[["cat", "bat"], ["float", "toalf"]]
+}
+
+console.log(anaGroup(["cat", "bat", "float", "toalf"]))
