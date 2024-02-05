228cf8f13349cd044276e683985ceca4810c040f
Mai Vang <vmaineng@gmail.com>
2024-02-05 09:54:28 -0800

    added in example

---
+++ febRedos/algo.js
@@ -0,0 +1,7 @@
+const longestRepeat = (s, k) => {
+    //a string of letters (uppercase), and an integer of how many times you can change it
+    //return the length of how many letters you can replace
+    //"apple", 1 => "ppple" => 3
+
+    //
+}
