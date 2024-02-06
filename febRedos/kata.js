e0b4f6ca8c6a648bdfcbff6e5bf073cfca45df10
Mai Vang <vmaineng@gmail.com>
2024-02-06 09:08:41 -0800

    added in kata

--- febRedos/kata.js
+++ febRedos/kata.js
@@ -2,3 +2,16 @@ function multiply(a, b){
     return a * b
    }
    
+
+   function simpleMultiplication(number) {
+    //receive a number
+   // if the output is even, return 8, otherwise 9
+   
+   //edge cases: if it's not a number
+   
+   //take the number and multiply by 8
+   //if the output is even, return 8; otherwise return 9
+   
+   return number % 2 === 0 ? number * 8 : number * 9;
+ }
+ 
