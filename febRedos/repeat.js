d2fc8593fb58a05c52b03af0a778826374835a06
Mai Vang <vmaineng@gmail.com>
2024-02-22 10:45:39 -0800

    added in flickswitch

--- febRedos/repeat.js
+++ febRedos/repeat.js
@@ -34,7 +34,7 @@ const longestRepeat = (s, k) => {
     maxLength = Math.max(maxLength, right - left + 1); // keeps track of length
     right++;  // ! after capturing longest length then can move teh right side of the window
 }
-  // return maxLength;
+  return maxLength;
 };
 
 // console.log(longestRepeat("APPLE", 1));
@@ -70,4 +70,96 @@ function twosum(num, target) {
       indexCheck[num[i]] = i 
     }
   }
+}
+
+function longRepeat(s, k) {
+  //receive a string of uppercase characters, and an integer to represent how many times you can switch out numbers
+  //return the longest substring containing the same letter
+  //"JUMPPER", 3 => "JUPPPPP" => 5
+
+  //keep count of how many letters
+  //create an object to store how many values have seen, length of what you have seen
+  //start at the first letter, keep adding values seen to the object
+  //can shrink window from the left if K amount of switches have occured,
+  //return the maxlength;
+
+let count = 0;
+let length = 0;
+let seenValues = {};
+
+let left = 0;
+let right = 0;
+
+while (left < right) {
+  const rightChar = s.charAt(right);
+  seenValues[rightChar] = seenValues[rightChar] + 1 || 1
+  count = Math.max(count, seenValues[rightChar])
+
+  if ((right - left + 1) - count > k) {
+    const leftChar = s.charAt(left)
+    seenValues[leftChar] -= 1
+    left++
+  }
+length = Math.max(length, (right - left + 1))
+right++
+}
+return length;
+
+}
+
+
+function checkParen(string){
+//receive a string of paranethesis
+//return true if paranethesis matches, else false
+//"{}([)" => true
+
+//create a stack
+//push on the parenthesis
+//if the parenthesis in the stack does not match the prior one, return false, otherwise, keep looking 
+//return true if stack is empty
+
+let checkStack = [];
+let addParen = 0;
+
+for (let i = 0; i < string.length;i++) {
+  if (string[i] === '(') {
+    checkStack.push(string[i])
+  } else if (string[i] === ')') {
+    if (checkStack.length > 0) {
+      checkStack.pop()
+    } else {
+      addParen++
+    }
+  }
+}
+addParen += checkStack.length
+return addParen
+
+}
+
+function flick(array) {
+  //an array of words that gets switched to opposite when 'flick'
+  //return the array with true or false
+  //['aapple', 'flick', 'jump', 'roar', 'flick', 'jill'] => [true, false, false false, true]
+
+  //set the flick to true
+  //go through the words and if you see flick, switch it, return the array
+
+  // let newArray = [];
+  let flickSwitch = true
+
+  return array.map((word) => {
+    if (word === 'flick') {
+      flickSwitch = !flickSwitch
+    }
+    return flickSwitch
+  })
+
+  // for (let i = 0; i < array.length; i++) {
+  //   if (array[i] === 'flick') {
+  //     flickSwitch = !flickSwitch
+  //     newArray.push(flickSwitch)
+  //   }
+  // }
+  // return newArray
 }
