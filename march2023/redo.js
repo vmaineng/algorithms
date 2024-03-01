9e0c324dd9996c855332ba45cde5ba29df3536cf
Mai Vang <vmaineng@gmail.com>
2024-03-01 10:10:59 -0800

    added in pseudo code

---
+++ march2023/redo.js
@@ -0,0 +1,19 @@
+function checkParen(string) {
+    //receive a string of parenthesis
+    //return true if all pairs, else false
+    //'))()' => false
+
+    //create an object
+    //store the opening as key, and closing as value
+
+    //iterate through the string
+    //check if it's an opening
+    //add on to stack
+    //else if it is a closing
+    //check to make sure the stack's length is not empty
+    //check the item from stack's length
+
+    //return true if stack's length is empty after checking
+
+    
+}
