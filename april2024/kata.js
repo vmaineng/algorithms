2c452c7329d19ab773efd3f38f79c3ac9b5c2b2e
Mai Vang <vmaineng@gmail.com>
2024-04-02 09:18:18 -0700

    added in minMax

--- april2024/kata.js
+++ april2024/kata.js
@@ -3,3 +3,20 @@ function arrayPlusArray(arr1, arr2) {
     //use reduce method on it
     return arr1.concat(arr2).reduce((accum, cv) => accum + cv, 0)
   }
+
+  function minMax(arr){
+    //receive an array of integers
+    //return [min, max] number from the array
+    //[5,6,3, 4, 0] => [0, 6]
+    
+    //edge case: if the length is one, return it as min and max
+    
+    //spread out the numbers for min, and max
+    //return it as an array
+    
+    if (arr.length === 1) return [arr[0], arr[0]];
+    
+    let min = Math.min(...arr);
+    let max = Math.max(...arr);
+    return [min, max]
+  }
