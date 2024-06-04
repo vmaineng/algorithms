be68e933104e75ea224f8c744ef9bd0160b3cf8d
Mai Vang <vmaineng@gmail.com>
2024-06-04 08:33:11 -0700

    added in kata

---
+++ june2024/kata.js
@@ -0,0 +1,19 @@
+function divisibleBy(numbers, divisor) {
+  //receive two array of integers (positive)
+  //return back an array of integers from numbers that are divisible by divisor
+  //[21, 36, 15], 3 => [21, 36, 15]
+
+  //if anything mod the divisor leaves no remainder, add to the output array
+
+  let outputArray = [];
+
+  for (let i = 0; i <= numbers.length; i++) {
+    if (numbers[i] % divisor === 0) {
+      outputArray.push(numbers[i]);
+    }
+  }
+  return outputArray;
+}
+
+//time: O(n)
+//space: O(n)
