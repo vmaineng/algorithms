12b2127d8aa982099227529c7434e0b34f085a87
Mai Vang <vmaineng@gmail.com>
2024-08-23 08:55:01 -0700

    added in longestPeak

--- august2024/meetingrooms.js
+++ august2024/meetingrooms.js
@@ -1,35 +1,147 @@
-export class Solution {
-  /**
-   * @param intervals: an array of meeting time intervals
-   * @return: if a person could attend all meetings
-   */
-  canAttendMeetings(intervals) {
-    //receive an array of meetings (start and end)
-    //return boolean; true if they can make it; else, false;
-    //[(3, 15), (4, 8)] => false since 15 overlaps with 4 - 8
+// export class Solution {
+//   /**
+//    * @param intervals: an array of meeting time intervals
+//    * @return: if a person could attend all meetings
+//    */
+//   canAttendMeetings(intervals) {
+//     //receive an array of meetings (start and end)
+//     //return boolean; true if they can make it; else, false;
+//     //[(3, 15), (4, 8)] => false since 15 overlaps with 4 - 8
 
-    //sort start time
-    //iterate through the intervals
-    //check if the end time is >= start time
-    //return false
-    //else return true
+//     //sort start time
+//     //iterate through the intervals
+//     //check if the end time is >= start time
+//     //return false
+//     //else return true
 
-    intervals.sort((a, b) => a.start - b.start);
+//     intervals.sort((a, b) => a.start - b.start);
 
-    for (let i = 0; i < intervals.length; i++) {
-      const [end] = intervals[i].end;
-      const [nextStart] = intervals[i + 1].start;
-      if (end > nextStart) {
-        return false;
-      }
+//     for (let i = 0; i < intervals.length; i++) {
+//       const [end] = intervals[i].end;
+//       const [nextStart] = intervals[i + 1].start;
+//       if (end > nextStart) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+// }
+
+// console.log(
+//   canAttendMeetings([
+//     [3, 15],
+//     [4, 8],
+//   ])
+// );
+
+function smallestDifference(arrayOne, arrayTwo) {
+  //receive two arrays of integers
+  //return a pair of integers that are closest to 0
+
+  //[3, 5, 2, 1]
+  //[7, 3, 2]
+
+  //[3, 3] = 0 => expectation of the return
+
+  //sort both arrays
+  //initialize a total to 0;
+  //create a pair array
+
+  //edge case: if both arrays are empty, return [];
+  //edge case: if one array is empty, return [];
+
+  //create pointers in both arrays starting at the first index
+  //check if sum is smaller than the current sum
+  //if so, add it in the pair array
+  //else check if value in the first array is greater than value in second array,
+  //if so, move the pointer accordingly
+  //return the pair
+
+  //objective:  trying to find two numbers that are as close as possible in value.
+
+  arrayOne.sort((a, b) => a - b);
+  arrayTwo.sort((a, b) => a - b);
+
+  let pair = [];
+  let difference = Number.MAX_SAFE_INTEGER;
+
+  let i = 0;
+  let j = 0;
+
+  while (i < arrayOne.length && j < arrayTwo.length) {
+    const oneValue = arrayOne[i];
+    const twoValue = arrayTwo[j];
+    let tempSum = Math.abs(oneValue - twoValue);
+    // console.log(tempSum, oneValue, twoValue);
+
+    if (tempSum < difference) {
+      difference = tempSum;
+      pair = [oneValue, twoValue];
     }
-    return true;
+    oneValue < twoValue ? i++ : j++; // ! bigger number to get the smaller difference
   }
+  return pair;
 }
 
-console.log(
-  canAttendMeetings([
-    [3, 15],
-    [4, 8],
-  ])
-);
+// Do not edit the line below.
+exports.smallestDifference = smallestDifference;
+
+console.log(smallestDifference([3, 5, 2, 1], [7, 3, 2]));
+
+function longestPeak(array) {
+  //receive an array of integers 9pos and negative
+  //return the length of longest peak (strictly increasing followed by a drop (decrease))
+
+  //edge case: if values < 3, can't make a peak (increasing and then  a decrease)
+
+  //best place to start with this algo is to find the peak;
+  //then look left and right to figure out length
+
+  //initialize peakTotal;
+  //initialize a pointer;
+
+  //iterate until we hit the last element
+
+  //check if it is a peak (value before it is smaller && value after is lower)
+  //use two pointer strategies
+  //left to check if value is still decreasing
+  //right to check if value is still decreasing
+  //then take index at right - left to update the length
+  //take the max version
+  //return the length
+
+  if (array.length < 3) return 0;
+
+  let longestPeakLength = 0;
+  let i = 1;
+
+  while (i < array.length - 1) {
+    const isPeak = array[i - 1] < array[i] && array[i] > array[i + 1];
+
+    if (!isPeak) {
+      i++;
+      continue;
+    }
+
+    let left = i - 2;
+
+    while (left >= 0 && array[left] < array[left + 1]) {
+      left--;
+    }
+
+    let right = i + 2;
+
+    while (right < array.length && array[right] < array[right - 1]) {
+      right++;
+    }
+
+    //right - left - 1 b/c the left and right pointer are accounted by + 2 outside of the loop
+    longestPeakLength = Math.max(longestPeakLength, right - left - 1);
+    i = right;
+  }
+
+  return longestPeakLength;
+}
+
+// Do not edit the line below.
+exports.longestPeak = longestPeak;
