633e49951e986316b0e435327d03cc28e2c283d8
Mai Vang <vmaineng@gmail.com>
2024-10-22 05:50:22 -0700

    added in spiral

--- oct2024/algo.js
+++ oct2024/algo.js
@@ -1633,3 +1633,50 @@ class BinaryTree {
   exports.BinaryTree = BinaryTree;
   exports.heightBalancedBinaryTree = heightBalancedBinaryTree;
   
+  function spiralTraverse(array) {
+    //receive an array of arrays
+     //return one array back
+   
+     //edge case, could one array be longer than the other?
+   
+     //start at the left corner and go around, 
+     //once you hit an end, decrement row down
+     //and column down
+   
+     let result = [];
+   let top = 0;
+     let bottom = array.length - 1;
+     let left = 0;
+     let right = array[0].length - 1;
+   
+     while (top <= bottom && left <= right) {
+       for (let i = left; i <= right; i++) {
+         result.push(array[top][i])
+       }
+       top++
+       
+       for (let i = top; i <= bottom; i++) {
+         result.push(array[i][right])
+       }
+       right--;
+   
+       if (top <= bottom) {
+         for (let i = right; i >= left; i--) {
+          result.push(array[bottom][i])
+         }
+         bottom--;
+       }
+   
+       if (left <= right) {
+         for (let i = bottom; i >= top; i--) {
+           result.push(array[i][left])
+         }
+         left++
+       }
+   }
+    return result
+   }
+   
+   // Do not edit the line below.
+   exports.spiralTraverse = spiralTraverse;
+   
