d5adccdbaa50ce25c141a5c898fcf624fa92002b
Mai Vang <vmaineng@gmail.com>
2024-11-02 09:36:24 -0700

    added in

--- nov2024/algo.js
+++ nov2024/algo.js
@@ -42,3 +42,67 @@ function twoNumberSum(array, targetSum) {
 
 // Do not edit the line below.
 exports.twoNumberSum = twoNumberSum;
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function (nums) {
+  //receive an unsorted array of integers
+  //return the length of the longest consecutive elements sequence
+  //[3, 2, 5, 6, 7] => [5,6, 7] => 3
+
+  //initialize a count
+  //sort the integers in the array
+  //check if the element in front is increasing by
+  //if it's a duplicate, continue
+  //if so, increment the count
+  //else if it isn't increasing
+  //capture the maxLength seen thus far
+  //return the maxLength
+
+  //edge case: if the nums array is less than one, it will always be the length of the array
+
+  // if (nums.length < 2) return nums.length
+
+  // let maxLength = 0;
+  // let currentLength = 1;
+
+  // nums.sort((a,b) => a -b);
+
+  // for (let i = 1; i < nums.length; i++) {
+  //     if (nums[i] === nums[i - 1]) continue;
+
+  //     if(nums[i] === nums[i-1] + 1) {
+  //         currentLength++
+  //     } else {
+  //         maxLength = Math.max(maxLength, currentLength)
+  //         currentLength = 1
+  //     }
+  // }
+  // maxLength = Math.max(maxLength, currentLength)
+  // return maxLength
+  //time: o(n log n);
+  //space: O(1)
+
+  //optimized using a set
+
+  if (nums.length === 0) return 0;
+
+  let consecVals = new Set(nums);
+  let maxLength = 0;
+
+  for (let num of consecVals) {
+    if (!consecVals.has(num - 1)) {
+      let currentNum = num;
+      let currentLength = 1;
+
+      while (consecVals.has(currentNum + 1)) {
+        currentLength++;
+        currentNum++;
+      }
+      maxLength = Math.max(maxLength, currentLength);
+    }
+  }
+  return maxLength;
+};
