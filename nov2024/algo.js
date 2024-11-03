8c57b841fc73cea76da1e71881ba5d2ae71c9cde
Mai Vang <vmaineng@gmail.com>
2024-11-02 19:13:22 -0700

    added in subarray

--- nov2024/algo.js
+++ nov2024/algo.js
@@ -106,3 +106,23 @@ var longestConsecutive = function (nums) {
   }
   return maxLength;
 };
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function (nums, k) {
+  let count = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    let sum = 0;
+    for (let j = i; j < nums.length; j++) {
+      sum += nums[j];
+      if (sum === k) {
+        count++;
+      }
+    }
+  }
+  return count;
+};
