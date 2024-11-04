56c5d57874f46a9bc5639332c6f543d302a25b4b
Mai Vang <vmaineng@gmail.com>
2024-11-04 12:37:20 -0800

    added in subarray

--- nov2024/algo.js
+++ nov2024/algo.js
@@ -328,3 +328,41 @@ var rotate = function (nums, k) {
 
   //time: O(n x k) => n is the length of nums and k is the number of rotations
 };
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function (nums, k) {
+  //receive an array of integers of nums and an integer k
+  //return total number of subarrays = k
+  //[1,4,3],5 => 1 b/c of [1,4]
+  //initialize a total sum
+  //iterate nums
+  //count up to k amount target
+  //if total = target, incremet count
+  // let count = 0;
+  // for (let i = 0; i < nums.length;i++) {
+  //     let sum = 0;
+  //     for (let j = i; j < nums.length; j++) {
+  //         sum += nums[j]
+  //         if (sum === k) {
+  //             count++
+  //         }
+  //     }
+  // }
+  // return count
+  // let count = 0;
+  // let sum = 0;
+  // const map = new Map();
+  // map.set(0,1);
+  // for (let num of nums) {
+  //     sum += num;
+  //     if (map.has(sum - k)) {
+  //         count += map.get(sum-k);
+  //     }
+  //     map.set(sum, (map.get(sum) || 0) + 1)
+  // }
+  // return count;
+};
