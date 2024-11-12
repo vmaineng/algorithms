b297b86405513f248782a580c56e68998fa2e5f0
Mai Vang <vmaineng@gmail.com>
2024-11-12 05:43:33 -0800

    added in binary

--- nov2024/algo.js
+++ nov2024/algo.js
@@ -932,3 +932,50 @@ var compress = function (chars) {
   }
   return answer.length;
 };
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  //receive an array of nums (possibly sorted), target (integer)
+  //return the index of target in nums
+
+  //[3,4,5,0,1,2] => 2 => 5
+
+  //create a left pointer
+  //create right pointer
+  //find the middle index
+  //check if the value at middle idx = target, return idx
+  //check if the array is still sorted from left to middle
+  //check if the target's value is in the left section
+  //if so, move right pointer down, else move left pointer to the other half
+  //else check if the target exists on the right side
+  //left pointer moves up, else right pointer moves down
+
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2);
+    if (nums[middle] === target) {
+      return middle;
+    }
+
+    if (nums[left] <= nums[middle]) {
+      if (nums[left] <= target && target <= nums[middle]) {
+        right = middle - 1;
+      } else {
+        left = middle + 1;
+      }
+    } else {
+      if (nums[middle] < target && target <= nums[right]) {
+        left = middle + 1;
+      } else {
+        right = middle - 1;
+      }
+    }
+  }
+  return -1;
+};
