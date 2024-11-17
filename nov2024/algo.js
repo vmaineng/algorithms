a3ef32aa0d15d59cd4823800d67c98f8cf599cdd
Mai Vang <vmaineng@gmail.com>
2024-11-17 09:44:29 -0800

    added in results

--- nov2024/algo.js
+++ nov2024/algo.js
@@ -1837,3 +1837,32 @@ var threeSum = function (nums) {
   }
   return answer;
 };
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function (nums1, nums2) {
+  //optimized method
+  //initialize a new array to capture results
+  //create a pointer at first index in each array
+  //iterate through arrays
+  //check to see if the values equal each other
+  //ask if the results array does not includes the array value
+  //if so push it in the arrays
+  //return arrays
+
+  let newResults = [];
+
+  for (let i = 0; i < nums1.length; i++) {
+    for (let j = 0; j < nums2.length; j++) {
+      if (nums1[i] === nums2[j]) {
+        if (!newResults.includes(nums1[i])) {
+          newResults.push(nums1[i]);
+        }
+      }
+    }
+  }
+  return newResults;
+};
