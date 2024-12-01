4d3de274dca8ec4455e155a7d114247e0a544e71
Mai Vang <vmaineng@gmail.com>
2024-11-30 16:24:19 -0800

    added in count

--- dec2024/algo.py
+++ dec2024/algo.py
@@ -3,3 +3,11 @@ def even_or_odd(number):
         return "Even"
     else:
         return "Odd"
+
+        def get_count(sentence):
+    vowels = 'aeiou'
+    count = 0
+    for letter in sentence:
+        if letter in vowels:
+            count += 1
+    return count
