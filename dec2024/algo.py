7f69fad56e8f371c6eb329674f89472058f81560
Mai Vang <vmaineng@gmail.com>
2024-12-01 14:34:55 -0800

    madded in py

--- dec2024/algo.py
+++ dec2024/algo.py
@@ -11,3 +11,11 @@ def even_or_odd(number):
         if letter in vowels:
             count += 1
     return count
+    def number_to_string(num):
+    #receive a number
+    # Return a string of the number here!
+    
+    #ex: 42 => '42'
+    #ex: -5 => '-5'
+    
+    return str(num)
