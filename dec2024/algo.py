c7b8cabdde69caae96033b63f1429dbde349c8c0
Mai Vang <vmaineng@gmail.com>
2024-12-10 06:06:10 -0800

    added in fizzbuzz

--- dec2024/algo.py
+++ dec2024/algo.py
@@ -186,8 +186,45 @@ def boolean_to_string(b):
     #add to list
     #return list
     
+    #using a for loop
     output = []
     for friend in x:
         if (len(friend)) == 4:
             output.append(friend)
     return output
+
+    #list comprehension
+    return [f for f in x if len(4) == 4]
+
+    #!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'fizzBuzz' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+def fizzBuzz(n):
+    # Write your code here
+    for i in range(1, n+1):
+        if i % 3 ==0 and i % 5 == 0:
+            print('FizzBuzz')
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    fizzBuzz(n)
