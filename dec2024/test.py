86bf9479afbe9f21e9f383f7568a03e0393d14f7
Mai Vang <vmaineng@gmail.com>
2024-12-15 16:14:39 -0800

    added in test

--- dec2024/test.py
+++ dec2024/test.py
@@ -1,24 +1,7 @@
-def double(list):
-    #receive a list of integers
-    #return a new list where the value are doubled
+def each_const(lst, n):
+    result = []
+    for i in range(len(lst) - n + 1):
+        result.append(lst[i:i+n])
+    return result
 
-    #ex: [1,2] => [2, 4]
-    #ex: [-3, 0, 3] => [-6, 0, 6]
-
-    #initialize a new array
-    #iterate through each value in the list
-    #multiply the value by 2
-    #add to the list
-    #return the new list
-
-    doubleList = []
-    for ele in list:
-        doubleNumber = ele *2
-        doubleList.append(doubleNumber)
-    return doubleList
-
-    #list comprehension
-    return [ele*2 for ele in list]
-
-result = double([1,2])
-print(result)
+print(each_const([12,3,4,5,6], 2))
