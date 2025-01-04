6775b87169e15dd0cc0c86a7b5a18c688303aba6
Mai Vang <vmaineng@gmail.com>
2025-01-04 08:56:02 -0800

    added in result

--- jan2025/algo.py
+++ jan2025/algo.py
@@ -396,3 +396,21 @@ class Solution:
                 left +=1
                 right +=1
         return ''.join(chars)
+
+        class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        #receive two lists of uneven lengths
+        #return a list of where they intersected
+
+        #ex: [3,4,5,2] and [2,3,4] => [3]
+
+        #brute force
+        #create two pointers
+        #check if they are the same value
+        #return the value as the list
+        result = set()
+        for i in range(len(nums1)):
+            for j in range(len(nums2)):
+                if nums1[i] == nums2[j]:
+                    result.add(nums1[i])
+        return list(result)
