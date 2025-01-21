a5cd2c79b2e04367731e741fe407ce05f4f2e550
Mai Vang <vmaineng@gmail.com>
2025-01-21 06:07:59 -0800

    added in algo

--- jan2025/algo.py
+++ jan2025/algo.py
@@ -1764,3 +1764,19 @@ class Solution:
             current = nextLL
         
         return prev
+
+        # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode], prev: Optional[ListNode]= None) -> Optional[ListNode]:
+        if not head:
+            return prev
+        nextLL = head.next
+        head.next = prev
+        return self.reverseList(nextLL, head)
+      
+        
+
