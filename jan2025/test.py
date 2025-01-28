b515fbd25b032e8a348f55980debecf03263ccd4
Mai Vang <vmaineng@gmail.com>
2025-01-28 06:09:02 -0800

    added in interval

--- jan2025/test.py
+++ jan2025/test.py
@@ -46,3 +46,32 @@ def min_meeting(intervals: List[List[int]]) -> int:
             minRooms -=1
             endPtr += 1
     return minRooms
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        #receive a list of list of integers
+        #return a list where all intervals overlapping merged together
+
+        #ex: [[1,4], [2, 5], [7,8]] => [[1,5], [7,8]]
+
+        #sort the list of integers based on start time
+        #create an empty list set with the first list from intervals
+        #iterate through the list of arrays starting at the second interval
+        #check if end time > start time
+        #update the end time of the interval
+        #else merge in the interval
+        #return new result
+
+        intervals.sort(key = lambda x:x[0])
+
+        result = [intervals[0]]
+
+        for i in range(1, len(intervals)):
+            [start, end] = intervals[i]
+            [prevStart, prevEnd] = result[-1]
+            if prevEnd >= start:
+                result[-1][1] = max(prevEnd, end)
+            else:
+                result.append(intervals[i]) 
+        return result
+        
