42b389925ae545c0d7dd2f4e8df95784b3203e77
Mai Vang <vmaineng@gmail.com>
2025-01-28 12:49:34 -0800

    added in insert interval

--- jan2025/test.py
+++ jan2025/test.py
@@ -104,3 +104,23 @@ class Solution:
                 results.append(intervals[i])
         return results
         
+
+        #insert intervals:
+        def insert_interval(intervals, new_interval):
+            result = []
+            i = 0
+            
+            while i < len(intervals) and intervals[i][1] < new_interval[0]:
+                result.append(intervals[i])
+                i += 1
+
+            while i < len(intervals) and intervals[i][0] <= new_interval[1]:
+                newInterval[0]= min(newInterval[0], intervals[i][0])
+                newInterval[1] = max(newInterval[1], intervals[i][1])
+                i += 1
+            result.append(new_interval)
+
+            while i < len(interval):
+                result.append(intervals[i])
+                i += 1
+            return result
