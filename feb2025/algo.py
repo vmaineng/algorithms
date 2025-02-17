fbe7a553e8ede415e226327d5194b81f1e2e37dc
Mai Vang <vmaineng@gmail.com>
2025-02-17 06:18:26 -0800

    added in stream

--- feb2025/algo.py
+++ feb2025/algo.py
@@ -409,3 +409,23 @@ class Solution:
                 result.append(i)
                 if len(result) ==k:
                     return result
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.nums = nums
+        
+
+    def add(self, val: int) -> int:
+        self.nums.append(val)
+        self.nums.sort(reverse=True)
+        return self.nums[self.k-1]
+    
+#time: O(n logn)
+#space:O(n)        
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
