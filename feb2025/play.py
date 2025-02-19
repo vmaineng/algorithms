bf4b1e656123cff2be08310eb2df307f39e76253
Mai Vang <vmaineng@gmail.com>
2025-02-19 12:36:30 -0800

    added in py

--- feb2025/play.py
+++ feb2025/play.py
@@ -33,8 +33,26 @@ b.append(40)
 a = [10,20,30]
 b = [10,20,30, 40]
 
+a = [10,20,30, 40] #reference same list object in memory
+b = [10,20,30, 40]
+
 x = 'hello'
 y = x
 x = 'world'
-y = 'hello'
+y = 'hello' #Strings are immutable, so when x is reassigned, y still holds "hello".
 
+data = {"score": 100}
+copy_data = data
+copy_data["score"] = 200
+data["score"] = 200
+#Since dictionaries are mutable and copy_data = data, both variables point to the same object in memory. Modifying copy_data affects data.
+
+b = a.copy()
+
+"Start", "Inside Timeout", "End"
+#Even though setTimeout(..., 0) has zero delay, it is still asynchronous and runs after the synchronous code finishes.
+
+"Waiting...", 'A was called'
+
+x.value = 10
+#Objects in JavaScript are reference types, so y = x makes y point to the same object. Modifying y.value also changes x.value.
