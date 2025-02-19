a611df21607a2b7add5142d73260834d2c0b4e68
Mai Vang <vmaineng@gmail.com>
2025-02-19 06:18:42 -0800

    added in top frequent elements

--- feb2025/algo.py
+++ feb2025/algo.py
@@ -602,3 +602,18 @@ class Solution:
                     return res
 
   
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        heap = []
+        counter = {}
+
+        for num in nums:
+            counter[num] = counter.get(num, 0) + 1
+        
+        for key, val in counter.items():
+            heapq.heappush(heap, (-val, key))
+
+        res = []
+        while len(res) < k:
+            res.append(heapq.heappop(heap)[1])
+        return res
