3ee21789db373b32ddb9595718ac7da5f7d9231b
Mai Vang <vmaineng@gmail.com>
2025-02-24 12:36:49 -0800

    added in trie

--- feb2025/topk.py
+++ feb2025/topk.py
@@ -81,3 +81,25 @@ def greet(name, owner):
 # param_2 = obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.empty()
+
+class Trie:
+
+    def __init__(self):
+        self.words = words
+
+    def insert(self, word: str) -> None:
+        return self.words.append(word)
+
+    def search(self, word: str) -> bool:
+        return word in self.words
+        #O (n * m)
+
+    def startsWith(self, prefix: str) -> bool:
+        return any(w.startswith(prefix) for w in self.words)
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
