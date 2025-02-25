df06926d908d13dd014df15bd5cc6bd171c2c233
Mai Vang <vmaineng@gmail.com>
2025-02-25 06:27:08 -0800

    added in pytho

--- feb2025/topk.py
+++ feb2025/topk.py
@@ -178,3 +178,72 @@ class MyStack:
 # param_2 = obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.empty()
+
+import random
+
+class RandomizedSet:
+
+    def __init__(self):
+        self.list = []
+
+    def insert(self, val: int) -> bool:
+        if val in self.list:
+            return False
+        self.list.append(val)
+        return True
+        
+
+    def remove(self, val: int) -> bool:
+        if val not in self.list:
+            return False
+        self.list.remove(val)
+        return True
+
+    def getRandom(self) -> int:
+        return random.choice(self.list)
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
+
+import random
+
+class RandomizedSet:
+
+    def __init__(self):
+        self.list = []
+        self.map = {}
+        
+
+    def insert(self, val: int) -> bool:
+        if val in self.map:
+            return False
+        self.list.append(val)
+        self.map[val] = len(self.list) - 1
+        return True
+
+    def remove(self, val: int) -> bool:
+        if val not in self.map:
+            return False
+        last_val = self.list[-1]
+        index = self.map[val]
+        self.list[index] = last_val
+        self.map[last_val] = index
+
+        self.list.pop()
+        del self.map[val]
+
+        return True
+
+    def getRandom(self) -> int:
+        return random.choice(self.list)
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
