ddc0b6f140107b007068f24060bce6c9d279e421
Mai Vang <vmaineng@gmail.com>
2025-02-25 06:09:39 -0800

    added in stack

--- feb2025/topk.py
+++ feb2025/topk.py
@@ -145,3 +145,36 @@ class MyStack:
 # param_2 = obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.empty()
+
+from collections import deque
+
+class MyStack:
+
+    def __init__(self):
+        self.q1 = deque()
+        self.q2 = deque()
+
+    def push(self, x: int) -> None:
+        self.q1.append(x)
+
+    def pop(self) -> int:
+        while len(self.q1) > 1:
+            self.q2.append(self.q1.popleft())
+        res = self.q1.popleft()
+        self.q1, self.q2 = self.q2, self.q1
+        return res
+
+    def top(self) -> int:
+        return self.q1[-1] if self.q1 else None
+        
+
+    def empty(self) -> bool:
+        return not self.q1
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
