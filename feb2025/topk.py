975dad3658f53ce862abdf21c7c72c5d0da5d0e6
Mai Vang <vmaineng@gmail.com>
2025-03-11 17:16:41 -0700

    added in memo

--- feb2025/topk.py
+++ feb2025/topk.py
@@ -701,7 +701,17 @@ class Solution:
         if n == 1:
             return 1
         return self.fib(n-1) + self.fib(n-2)
+        class Solution:
+    def fib(self, n: int, memo={}) -> int:
+        if n in memo:
+            return memo[n]
         
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        memo[n] = self.fib(n-1, memo) + self.fib(n - 2, memo)
+        return memo[n]
 
     
     
