361e15d29e7d0173162f9990cd198e46533e975d
Mai Vang <vmaineng@gmail.com>
2025-03-28 12:58:42 -0700

    added in dll

--- feb2025/topk.py
+++ feb2025/topk.py
@@ -1322,3 +1322,40 @@ def move(position, roll):
     
 #time: O(n) for n amount of char in s
 #space:O(1)
+
+class Node:
+    def __init__(self, url: str):
+        self.prev = None
+        self.next = None
+        self.url = url
+
+class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        self.curr = Node(homepage)
+
+    def visit(self, url: str) -> None:
+        new_node = Node(url)
+        self.curr.next = new_node
+        new_node.prev = self.curr
+        self.curr = new_node #move to new page
+
+    def back(self, steps: int) -> str:
+        while steps >0 and self.curr.prev:
+            self.curr = self.curr.prev
+            steps -= 1
+        return self.curr.url
+
+    def forward(self, steps: int) -> str:
+        while steps > 0 and self.curr.next:
+            self.curr = self.curr.next
+            steps -= 1
+        return self.curr.url
+        
+
+
+# Your BrowserHistory object will be instantiated and called as such:
+# obj = BrowserHistory(homepage)
+# obj.visit(url)
+# param_2 = obj.back(steps)
+# param_3 = obj.forward(steps)
