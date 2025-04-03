1fa1dc5bb950599111cfd7f6b0139aee8a82c142
Mai Vang <vmaineng@gmail.com>
2025-04-03 06:35:01 -0700

    added in inordertraversal

--- march2025/algo.py
+++ march2025/algo.py
@@ -25,3 +25,27 @@ class BST:
         self.value = value
         self.left = None
         self.right = None
+
+def findClosestValueInBst(tree, target):
+    def inOrderTraversal(node):
+        if node is None:
+            return []
+        return inOrderTraversal(node.left) + [node.value] + inOrderTraversal(node.right)
+
+    all_values = inOrderTraversal(tree)
+    closest = None
+    min_diff = float('inf')
+    for value in all_values:
+        current_diff = abs(value - target)
+        if current_diff < min_diff:
+            min_diff = current_diff
+            closest = value
+    return closest
+
+
+# This is the class of the input tree. Do not edit.
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
