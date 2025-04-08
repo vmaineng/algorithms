296d498b69b43e770b82b12c262e26b7bf3d005f
Mai Vang <vmaineng@gmail.com>
2025-04-08 11:35:20 -0700

    added in git push

--- march2025/algo.py
+++ march2025/algo.py
@@ -57,3 +57,29 @@ class Solution:
         
     def decode(self, s: str) -> List[str]:
         return s.split('#')
+def inOrderTraverse(tree, array):
+    #starts from bottom left, to top, to bottom right
+    if tree is not None:
+        inOrderTraverse(tree.left, array)
+        array.append(tree.value)
+        inOrderTraverse(tree.right, array)
+    return array
+    
+
+
+def preOrderTraverse(tree, array):
+    # starts from root, to left, to right
+    if tree is not None:
+        array.append(tree.value)
+        preOrderTraverse(tree.left, array)
+        preOrderTraverse(tree.right, array)
+    return array
+
+
+def postOrderTraverse(tree, array):
+    # bottom left, to bottom right, to top
+    if tree is not None:
+        postOrderTraverse(tree.left, array)
+        postOrderTraverse(tree.right, array)
+        array.append(tree.value)
+    return array
