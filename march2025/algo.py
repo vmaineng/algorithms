7d003dca89ef333e5633bc21401e310501945c86
Mai Vang <vmaineng@gmail.com>
2025-04-09 09:32:11 -0700

    added in bst

--- march2025/algo.py
+++ march2025/algo.py
@@ -260,3 +260,27 @@ class BST:
         self.value = value
         self.left = None
         self.right = None
+def findClosestValueInBst(tree, target):
+    return findClosest(tree, target, tree.value)
+
+def findClosest(tree, target, closest):
+    if tree is None:
+        return closest
+
+    if abs(target - closest) > abs(target - tree.value):
+        closest = tree.value
+
+    if target < tree.value:
+        return findClosest(tree.left, target, closest)
+    elif target > tree.value:
+        return findClosest(tree.right, target, closest)
+    else:
+        return closest
+
+
+# This is the class of the input tree. Do not edit.
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
