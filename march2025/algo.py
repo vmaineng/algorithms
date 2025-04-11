3cdd71d4c592278b1e61735b19b052485f56fa3f
Mai Vang <vmaineng@gmail.com>
2025-04-11 06:46:24 -0700

    added in match

--- march2025/algo.py
+++ march2025/algo.py
@@ -332,3 +332,17 @@ def validate_pin(pin):
 #         return True
 #     else:
 #         return False
+
+def get_planet_name(id):
+    # This doesn't work; Fix it!
+    name=""
+    match id:
+        case 1: name = "Mercury"
+        case 2: name = "Venus"
+        case 3: name = "Earth"
+        case 4: name = "Mars"
+        case 5: name = "Jupiter"
+        case 6: name = "Saturn"
+        case 7: name = "Uranus"  
+        case 8: name = "Neptune"
+    return name
