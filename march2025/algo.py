750464eebbc2ea3d40f280325826ec29a58c6cca
Mai Vang <vmaineng@gmail.com>
2025-04-12 07:59:11 -0700

    added in call stack

--- march2025/algo.py
+++ march2025/algo.py
@@ -635,3 +635,37 @@ class BST:
 #to go from iterative to recursive: 
 #use the properties of left to assign all left nodes
 #use right to assign all right notes
+
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        #receive a grid of m x n 
+        #return max area of an island in grid
+
+        #iterate through row and cols
+        #if the value is a 1, perform dfs
+        #return the size from dfs
+
+        #edge case: if grid is empty, return 0
+
+        rows = len(grid)
+        cols = len(grid[0])
+        max_area = 0
+
+        def dfs(row, col):
+            if row < 0 or row >= len(grid) or col < 0 or col >= len(grid[0]) or grid[row][col] == 0:
+                return 0
+            grid[row][col] = 0
+            size = 1
+            size += dfs(row + 1, col)
+            size += dfs(row - 1, col)
+            size += dfs(row, col - 1)
+            size += dfs(row, col + 1)
+            return size
+
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == 1:
+                    size = dfs(row, col)
+                    max_area = max(max_area, size)
+        return max_area
+        
