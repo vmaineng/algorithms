e0803caf1cc117bfb01eb14aa5f955adb1e2ba3b
Mai Vang <vmaineng@gmail.com>
2025-04-12 08:30:46 -0700

    added in browser

--- march2025/algo.py
+++ march2025/algo.py
@@ -703,4 +703,33 @@ class Solution:
                     max_area = max(max_area, size)
         return max_area
         
+        class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        self.history = [homepage]
+        self.count = 0
+
+    def visit(self, url: str) -> None:
+        self.history = self.history[:self.count + 1]
+        self.history.append(url)
+        self.count += 1
+        
+
+    def back(self, steps: int) -> str:
+        #go back up to idx of 0, and where we are currently
+        self.count = max(0, self.count - steps)
+        return self.history[self.count]
+        
+
+    def forward(self, steps: int) -> str:
+        self.count = min(len(self.history) - 1, self.count + steps)
+        return self.history[self.count]
+        
+
+
+# Your BrowserHistory object will be instantiated and called as such:
+# obj = BrowserHistory(homepage)
+# obj.visit(url)
+# param_2 = obj.back(steps)
+# param_3 = obj.forward(steps)
         
