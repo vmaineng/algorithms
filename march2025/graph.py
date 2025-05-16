634940ed1abc2808a75e109db99e1d6be3192148
Mai Vang <vmaineng@gmail.com>
2025-05-16 07:53:08 -0700

    added in shortest path

--- march2025/graph.py
+++ march2025/graph.py
@@ -434,4 +434,39 @@ def has_size(graph, current, dst, visited):
     size += has_size(graph, neighbor, dst, visited)
   return size
     
+    from collections import deque
+
+def shortest_path(edges, node_A, node_B):
+  #receive an edge list, 
+  #return count of path between two nodes - (the edges)
+
+  #create a visited set for undriected
+  #use a queue to iterate through
+  #pass in coordinates and count
+  #return count if reached second node
+
+  graph = build_graph(edges)
+  queue = deque([ (node_A, 0) ])
+  visited = set([node_A])
+  while queue:
+    node, distance = queue.popleft()
+    if node ==node_B:
+      return distance
+    for neighbor in graph[node]:
+      if neighbor not in visited:
+        visited.add(neighbor)
+        queue.append((neighbor, distance + 1))
+  return -1
+
+def build_graph(edges):
+  graph = {}
+  for edge in edges:
+    a,b = edge
+    if a not in graph:
+      graph[a] = []
+    if b not in graph:
+      graph[b] = []
+    graph[a].append(b)
+    graph[b].append(a)
+  return graph
   
