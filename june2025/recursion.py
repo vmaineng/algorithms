f9d35984a759bd0634235915170157a964c354db
Mai Vang <vmaineng@gmail.com>
2025-06-17 07:57:48 -0700

    added in recursion

---
+++ june2025/recursion.py
@@ -0,0 +1,25 @@
+# Tip: You can use the type(element) function to check whether an item
+# is a list or an integer.
+def productSum(array, multiplier = 1):
+    # receive a list of lists of numbers
+    #return the total of all numbs together
+
+    #[4[3,8]] => 4 + 2 * (8 +3) 4+ 22 => 26
+
+    #iterate through each eelemnt of the list
+    #check if it is a list
+    #then add all the numbers, increase the multiplei
+    #else, it's not a list, then add all the numbers together
+    #add sum together
+
+    total = 0
+    if not array:
+        return 0
+
+    for element in array:
+        if type(element) is list:
+            total += productSum(element, multiplier + 1)
+        else:
+            total += element
+    return total * multiplier
+    
