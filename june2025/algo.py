7daedecea786852284f3eb8f4d61877152793ab1
Mai Vang <vmaineng@gmail.com>
2025-07-20 09:04:11 -0700

    added in algo

--- june2025/algo.py
+++ june2025/algo.py
@@ -1556,6 +1556,13 @@ class Solution:
         return maxCount
         
         
+        def correct_tail(body, tail):
+#     sub = body[-1:]
+#     if sub == tail:
+#         return True
+#     else:
+#         return False
+    return True if body[-1:] == tail else False
 
     
     
