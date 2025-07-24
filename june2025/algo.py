0ba7b3fc5629bdea6d0a6f520a85607edbebdb0a
Mai Vang <vmaineng@gmail.com>
2025-07-24 13:13:10 -0700

    added int erinary

--- june2025/algo.py
+++ june2025/algo.py
@@ -1701,4 +1701,7 @@ class Solution:
         return count
 
 
-        
+        def how_many_dalmatians(number):
+    dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIONS!!!"];
+    
+    return dogs[0] if number <= 10 else (dogs[1] if number <= 50 else (dogs[3] if number == 101 else dogs[2]))
