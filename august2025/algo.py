7dbe559b2b2bc190fb79ae42675a26c28c8a0059
Mai Vang <vmaineng@gmail.com>
2025-08-08 12:23:53 -0500

    added in algo

--- august2025/algo.py
+++ august2025/algo.py
@@ -16,4 +16,27 @@ def nearest_sq(n):
 #time: O(1) => computing one number
 #space: O(1) => returning a number
     
+    def get_drink_by_profession(param):
+    #receive a string of letters
+    #return the corresponding output from the input
     
+    #ex: 'schOOL counsElor' => 'Anything with Alcohol'
+    
+    #brute force with if-else statement
+    #check the params.toLowerCase == 'jabroni' => return 'Patron Tequila'
+    #repeat for the rest of the items
+    
+    
+    #optimized way => capture the following inputs and outputs in an object
+    
+    drinks = { 
+    'jabroni': 'Patron Tequila',
+        'school counselor': "Anything with Alcohol",
+        'programmer': "Hipster Craft Beer",
+        'bike gang member': "Moonshine",
+        'politician': "Your tax dollars",
+        'rapper': 'Cristal',
+    }
+    
+#     return drinks[param.lower()] if drinks[param.lower()] else "Beer"
+    return drinks.get(param.lower(), "Beer")
