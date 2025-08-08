b951810ddf70390261bbb43924febb9b3c2476d8
Mai Vang <vmaineng@gmail.com>
2025-08-08 12:48:01 -0500

    added in recentcounter

--- august2025/algo.py
+++ august2025/algo.py
@@ -64,3 +64,26 @@ class RecentCounter:
 #receive recent requests
 #intiailize a counter with 0 recent requests
 #
+
+from collections import deque
+
+class RecentCounter:
+
+    def __init__(self):
+        self.requests = deque()
+
+    def ping(self, t: int) -> int:
+        self.requests.append(t)
+
+        while self.requests[0] < t-3000:
+            self.requests.popleft()
+        return len(self.requests)
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
+
+#receive recent requests
+#intiailize a counter with 0 recent requests
+#
