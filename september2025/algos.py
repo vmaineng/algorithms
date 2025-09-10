46207a3d48d33fd9650cf247468a275255ca0678
Mai Vang <vmaineng@gmail.com>
2025-09-10 06:58:16 -0700

    added in algo

--- september2025/algos.py
+++ september2025/algos.py
@@ -492,4 +492,22 @@ class Solution:
             return good
         return dfs(root, root.val)
         
+        # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        def dfs(node, max_so_far):
+            if not node:
+                return 0
+            good = 1 if node.val >= max_so_far else 0
+            max_seen = max(max_so_far, node.val)
+            
+            good += dfs(node.left, max_seen)
+            good += dfs(node.right, max_seen)
+            return good
+        return dfs(root, root.val)
         
