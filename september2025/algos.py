86e84ee5f7dab90fdc71d9af19beafd9c70de552
Mai Vang <vmaineng@gmail.com>
2025-09-28 15:29:33 -0700

    added in spiral

--- september2025/algos.py
+++ september2025/algos.py
@@ -859,3 +859,29 @@ class Solution:
                 return True
 
         return len(visited) == n
+    
+    class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        top, bottom = 0, len(matrix) -1
+        left, right = 0, len(matrix[0]) - 1
+        result = []
+
+        while top <= bottom and left <= right:
+            for i in range(left, right + 1):
+                result.append(matrix[top][i])
+            top += 1
+
+            for i in range(top, bottom + 1):
+                result.append(matrix[i][right])
+            right -= 1
+
+            if top <= bottom: 
+                for i in range(right, left - 1, -1):
+                    result.append(matrix[bottom][i])
+                bottom -= 1
+            
+            if left <= right:
+                for i in range(bottom, top - 1, -1): 
+                    result.append(matrix[i][left])
+                left += 1
+        return result
