6b08f6bf4728e0fc05f877ea80541d6a2df9c57f
Mai Vang <vmaineng@gmail.com>
2025-10-02 09:17:59 -0700

    added in dfs

--- september2025/dict.py
+++ september2025/dict.py
@@ -84,3 +84,30 @@ class Solution:
                 if grid[row][col] == 1: 
                     count = max(count, dfs(row, col))
         return count
+    
+    class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        #receive a m x n grid
+        #return the max area 
+
+        #if the cell is 1, traverse dfs and add to count
+        #return max count seen
+
+        max_area = 0
+        rows, cols = len(grid), len(grid[0])
+
+        def dfs(row, col):
+            if row < 0 or col < 0 or row >= rows or col >= cols or grid[row][col] == 0:
+                return 0
+            grid[row][col] = 0
+            return (1 + 
+            dfs(row - 1, col) + 
+            dfs(row + 1, col) + 
+            dfs(row, col - 1) + 
+            dfs(row, col + 1))
+
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == 1:
+                    max_area = max(max_area, dfs(row, col))
+        return max_area
