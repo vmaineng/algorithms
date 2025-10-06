3481bd6d97e795b817e706cd35225c8c4995eb38
Mai Vang <vmaineng@gmail.com>
2025-10-06 12:03:24 -0500

    added in path

--- september2025/algos.py
+++ september2025/algos.py
@@ -1030,3 +1030,47 @@ def has_path(graph, src, dst, visited):
     if has_path(graph, neighbor, dst, visited):
       return True
   return False
+
+def undirected_path(edges, node_A, node_B):
+  #build a graph into a matrix list
+  #traverse through it 
+  #return true or false
+
+  graph = build_graph(edges)
+  return has_path(graph, node_A, node_B, set())
+
+def build_graph(edges):
+  graph = {}
+
+  for edge in edges:
+    a, b = edge
+
+    if a not in graph:
+      graph[a] = []
+
+    if b not in graph:
+      graph[b] = []
+
+    graph[a].append(b)
+    graph[b].append(a)
+
+  return graph
+
+def has_path(graph, src, dst, visited):
+  if src == dst:
+    return True
+
+  if src in visited:
+    return False
+
+  visited.add(src)
+
+  for neighbor in graph[src]:
+    if has_path(graph, neighbor, dst, visited) == True:
+      return True
+  return False
+  
+
+
+
+  
