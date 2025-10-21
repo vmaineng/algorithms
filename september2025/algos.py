7933c9898cd6cb38fa6afc674eabbc4c0611f9d4
Mai Vang <vmaineng@gmail.com>
2025-10-21 11:44:22 -0500

    added in island

--- september2025/algos.py
+++ september2025/algos.py
@@ -1317,3 +1317,35 @@ class Solution:
                     dfs(row, col)
                     count += 1
         return count
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        #receive a m x n grid
+        #return count of islands
+
+        #iterate through rows and cols
+        #check to see if it is a 1, then dfs and mark it as visited
+        #return count
+
+        if not grid or not grid[0]:
+            return 0
+
+        rows, cols = len(grid), len(grid[0])
+        count = 0
+
+        def dfs(row, col):
+            if row < 0 or col < 0 or row >= rows or col >= cols or grid[row][col] == '0':
+                return
+            grid[row][col] = '0'
+            dfs(row + 1, col)
+            dfs(row -1, col)
+            dfs(row, col + 1)
+            dfs(row, col -1)
+
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == '1':
+                    dfs(row, col)
+                    count += 1
+        return count
+
