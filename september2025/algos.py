119872d687a97690fca8327431e25ab71d5d4ac0
Mai Vang <vmaineng@gmail.com>
2025-10-27 11:57:23 -0500

    added in graph

--- september2025/algos.py
+++ september2025/algos.py
@@ -1390,3 +1390,33 @@ def difference_in_ages(ages):
     youngest = min(ages)
     difference = oldest-youngest
     return (youngest, oldest, difference)
+
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        rows, cols = len(board), len(board[0])
+
+        def dfs(r,c):
+            if r < 0 or c < 0 or r >= rows or c >= cols or board[r][c] != '0':
+                return
+            
+            board[r][c] = 'S'
+            dfs(r +1, c)
+            dfs(r-1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+        
+        for r in range(rows):
+            for c in [0, cols-1]:
+                if board[r][c] == '0':
+                    dfs(r,c)
+        for c in range(cols):
+            for r in [0, rows-1]:
+                if board[r][c] == '0':
+                    dfs(r,c)
+
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == '0':
+                    board[r][c] = 'X'
+                elif board[r][c] == 'S':
+                    board[r][c] = '0'
