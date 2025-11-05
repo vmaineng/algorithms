2fc596e1daed4be303172ab5fd4c4f68eb44bf5e
Mai Vang <vmaineng@gmail.com>
2025-11-05 10:44:43 -0600

    added in graph

--- nov2025/graphs.py
+++ nov2025/graphs.py
@@ -44,3 +44,39 @@ def has_path(graph, src, dst):
     if (has_path(graph, neighbor, dst) == True):
       return True
   return False
+
+def undirected_path(edges, node_A, node_B):
+  #TURN PATH INTO adjency matrix
+  #then check if it has PATH
+  graph = build_graph(edges)
+  return has_path(graph, node_A, node_B, set())
+
+def build_graph(edges):
+  graph = {}
+
+  for a,b in edges: 
+    if a not in graph:
+      graph[a] = []
+  
+    if b not in graph:
+      graph[b] = []
+  
+    graph[a].append(b)
+    graph[b].append(a)
+
+  return graph
+
+def has_path(graph, src, dst, visited):
+  if src == dst:
+    return True
+
+  if src in visited:
+    return False
+  
+  visited.add(src)
+
+  for neighbor in graph[src]:
+    if has_path(graph, neighbor, dst, visited) == True:
+      return True
+  return False
+    
