d58b642b20b4e18258a87eecc2ff0020a893213b
Mai Vang <vmaineng@gmail.com>
2025-11-05 11:16:24 -0600

    added in explore

--- nov2025/graphs.py
+++ nov2025/graphs.py
@@ -118,3 +118,23 @@ def has_path(graph, src, dst, visited):
     if has_path(graph, neighbor, dst, visited) == True:
       return True
   return False
+
+def connected_components_count(graph):
+  visited = set()
+  count = 0
+
+  for node in graph:
+    if explore(graph, node, visited) == True:
+      count += 1
+
+  return count
+
+def explore(graph, current, visited):
+  if current in visited:
+    return False
+
+  visited.add(current)
+
+  for neighbor in graph[current]:
+    explore(graph, neighbor, visited)
+  return True
