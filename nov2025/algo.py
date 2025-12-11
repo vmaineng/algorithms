58ec74290e80ad5a79e0dd94e122af1d871b3c1d
Mai Vang <vmaineng@gmail.com>
2025-12-11 10:25:51 -0800

    added in deque

--- nov2025/algo.py
+++ nov2025/algo.py
@@ -289,3 +289,33 @@ def tree_sum(root):
     return 0
 
   return root.val + tree_sum(root.left) + tree_sum(root.right)
+
+
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.left = None
+#     self.right = None
+
+from collections import deque
+
+def tree_includes(root, target):
+  #receive the root node, and a target node
+  #return True if node exists, else False
+
+  #visit bfs
+
+  if not root:
+    return False
+
+  queue = deque([root])
+  while queue:
+    current = queue.popleft()
+    if current.val == target:
+      return True
+
+    if current.left:
+      queue.append(current.left)
+    if current.right:
+      queue.append(current.right)
+  return False
