66380de212ba7633e994110c0c063c921e8b11f8
Mai Vang <vmaineng@gmail.com>
2025-12-17 08:52:49 -0800

    added in algo

--- nov2025/algo.py
+++ nov2025/algo.py
@@ -680,3 +680,42 @@ def build_graph(edges):
     graph[a].append(b)
     graph[b].append(a)
   return graph
+
+from collections import deque
+
+def shortest_path(edges, node_A, node_B):
+  #receive an adj list
+  #return min distance
+  #ex:
+
+  #create an adj matrix
+  #traverse through queue
+  #visited - keep track of nodes seen
+  #queue to travel
+
+  graph = build_graph(edges)
+  queue = deque([ (node_A, 0) ])
+  visited = set([node_A])
+
+  while queue:
+    node, distance = queue.popleft()
+    if node == node_B:
+      return distance
+
+    for neighbor in graph[node]:
+      if neighbor not in visited:
+        visited.add(neighbor)
+        queue.append((neighbor, distance + 1))
+  return -1
+
+def build_graph(edges):
+  graph = {}
+
+  for a,b in edges:
+    if a not in graph:
+      graph[a] = []
+    if b not in graph:
+      graph[b] = []
+    graph[a].append(b)
+    graph[b].append(a)
+  return graph
