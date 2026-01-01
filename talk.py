74a4b599f1c87011f7322dfbd4ba05cf488fa06f
Mai Vang <vmaineng@gmail.com>
2025-12-31 16:47:09 -0800

    added in list

--- talk.py
+++ talk.py
@@ -20,5 +20,23 @@ def countdown(n):
         return 
     print(n) #5, 4
     return countdown(n - 1) # 5- 1 = 4, # 4-1 3
-countdown(5)
+# countdown(5)
 #time: O(n), space: O(n)
+
+def sumlist(lst):
+    #receive a list of integers
+    #return the total sum of integers
+    #ex: [2, 3, -3, 5,7] => 14
+    #sum: 2 + [3, -3, 5, 7]
+    #sum: 2+ 3 + [-3, 5, 7]
+
+    #base case: if length of array is empty: return 
+    #add each number to the total from each function call
+
+    if len(lst) == 0:
+        return 0
+    return lst[0] + sumlist(lst[1:])
+    
+print(sumlist([2, 3, -3, 5,7]))
+
+#time: O(n^2) ; space: O(n)
