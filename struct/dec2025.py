d72b716971382f0720020d0bfaaf1da605029d87
Mai Vang <vmaineng@gmail.com>
2026-01-08 09:58:33 -0800

    added in dfs

--- struct/dec2025.py
+++ struct/dec2025.py
@@ -1359,4 +1359,18 @@ def path_finder(root, target):
   return None
   
 
-  
+  # class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.left = None
+#     self.right = None
+
+def tree_value_count(root, target):
+  #receive the root of a tree, and a target tree_value_count
+  #return count of how many times the value occurred
+
+  if not root:
+    return 0
+
+  count = 1 if root.val == target else 0
+  return count + tree_value_count(root.left, target) + tree_value_count(root.right, target)
