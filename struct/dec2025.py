781972e3e561b55a3dad2681a1b214f721302ab3
Mai Vang <vmaineng@gmail.com>
2026-01-09 09:33:12 -0800

    added in queue

--- struct/dec2025.py
+++ struct/dec2025.py
@@ -1496,3 +1496,24 @@ def how_high(root):
     return -1
 
   return 1 + max(how_high(root.left), how_high(root.right))
+
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.left = None
+#     self.right = None
+
+from collections import deque
+
+def bottom_right_value(root):
+  queue = deque([root])
+
+  while queue:
+    current = queue.popleft()
+
+    if current.left:
+      queue.append(current.left)
+
+    if current.right:
+      queue.append(current.right)
+  return current.val
