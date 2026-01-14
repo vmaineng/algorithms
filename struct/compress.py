2c47c0522ab6711fea2605e8d02a5a278c442f5c
Mai Vang <vmaineng@gmail.com>
2026-01-14 08:19:30 -0800

    added in connected c ompone

--- struct/compress.py
+++ struct/compress.py
@@ -308,3 +308,32 @@ def has_path(graph, src, dst, visited):
     if has_path(graph, neighbor, dst, visited) == True:
       return True
   return False
+
+def connected_components_count(graph):
+  #keep count 
+  #iterate through neighbor
+  #keep track of nodes seen
+
+
+
+  count = 0
+  visited = set()
+
+
+  for node in graph: 
+    if explore(graph, node, visited):
+      count += 1
+  return count
+
+def explore(graph, current, visited):
+  if current in visited:
+    return False
+
+  visited.add(current)
+  
+  for neighbor in graph[current]:
+    explore(graph,neighbor, visited)
+    
+  return True
+
+  
