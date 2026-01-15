9253ea9f95cc3a4287937878a9c86bce5507e5ec
Mai Vang <vmaineng@gmail.com>
2026-01-15 10:11:36 -0800

    added in dec

--- struct/dec2025.py
+++ struct/dec2025.py
@@ -1862,3 +1862,44 @@ def explore(graph, node, visited):
     size += explore(graph, neighbor, visited)
   return size
     
+    from collections import deque
+
+def shortest_path(edges, node_A, node_B):
+  #receive an edges
+  #return shortest path to from a to b
+  #ex: 
+
+  #do it via bfs using a queue
+  #modify edges into graph 
+
+
+  graph = build_graph(edges)
+  visited = set([node_A])
+  queue = deque( [(node_A, 0 )] )
+
+  while queue:
+    current, dist = queue.popleft()
+
+    if current == node_B:
+      return dist
+
+    for neighbor in graph[current]:
+      if neighbor not in visited:
+        visited.add(neighbor)
+        queue.append((neighbor, dist + 1))
+    
+  return -1
+  
+
+def build_graph(edges):
+  graph = {}
+
+  for a,b in edges:
+    if a not in graph:
+      graph[a] = []
+    if b not in graph:
+      graph[b] = []
+    graph[a].append(b)
+    graph[b].append(a)
+  return graph 
+  
