c7fadb2ba66d7899d5f55006bb899e53da82df3d
Mai Vang <vmaineng@gmail.com>
2026-01-16 09:36:40 -0800

    added in water

--- struct/compress.py
+++ struct/compress.py
@@ -434,3 +434,37 @@ def explore(graph, node, visited):
         
 #     return "".join(result)
     return st.replace('!', '') + "!"
+  
+
+  def minimum_island(grid):
+  #receive a row by col grid
+  #return smallest size island available
+  #ex: 
+
+  #keep track of smallest size
+
+  smallest = float('inf')
+  visited = set()
+
+  for row in range(len(grid)):
+    for col in range(len(grid[0])):
+      size = dfs(grid, row, col, visited)
+      if size < smallest and size > 0:
+        smallest = size
+  return smallest
+
+
+def dfs(grid, row, col, visited):
+  if row < 0 or row >= len(grid) or col < 0 or col >= len(grid[0]) or (row, col) in visited or grid[row][col] == 'W':
+    return 0
+
+  visited.add((row, col))
+
+  size = 1
+  
+  size += dfs(grid, row -1, col, visited)
+  size += dfs(grid, row + 1, col, visited)
+  size += dfs(grid, row, col - 1, visited)
+  size += dfs(grid, row, col + 1, visited)
+  return size
+  
