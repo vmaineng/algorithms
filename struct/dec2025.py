f8fb5c5fb3acc8bc56e72b1c0a4f2945ca87872c
Mai Vang <vmaineng@gmail.com>
2026-01-27 09:32:04 -0800

    added in stack

--- struct/dec2025.py
+++ struct/dec2025.py
@@ -2396,6 +2396,35 @@ def reverse_some_chars(s, chars):
       
   return ''.join(result)
       
+
+      def reverse_some_chars(s, chars):
+  #receive a string  of lowercase chars, and a list of chars 
+  #return the string reversed if the char is in list
+  #ex: 'apple', ['a', 'e'] => 'eppla'
+
+  #initialize an empty list
+  #iterate through each char to check if it is in chars
+  #add it to a separate list (can add to stack)
+  #return new string
+
+  stack = []
+  result = []
+
+  for char in s:
+    if char in chars:
+      stack.append(char)
+    # print(stack) ['o', 'u', 'e'] 
+
+  for char in s:
+    if char in chars:
+      result.append(stack.pop())
+    else:
+      result.append(char)
+  print(result)
+      
+  return ''.join(result)
+      
+  
   
   
       
