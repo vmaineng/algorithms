59e766fb1fb4a5aabd3f86c867f35b1b3a58ac37
Mai Vang <vmaineng@gmail.com>
2026-01-29 09:56:11 -0800

    added in memo pos

--- struct/dec2025.py
+++ struct/dec2025.py
@@ -2680,6 +2680,27 @@ def _count_paths(grid, r ,c, memo):
   memo[pos] = down_paths + right_paths
   return memo[pos]
 
+def max_path_sum(grid):
+  return _max_path_sum(grid, 0,0, {})
+
+def _max_path_sum(grid, r, c, memo):
+  pos = (r, c)
+  if pos in memo:
+    return memo[pos]
+    
+  if r == len(grid) or c == len(grid[0]):
+    return float("-inf")
+
+  if r == len(grid) -1  and c == len(grid[0]) -1:
+    return grid[r][c]
+
+  down_paths = _max_path_sum(grid, r + 1, c, memo)
+  right_paths = _max_path_sum(grid, r, c + 1, memo)
+  memo[pos] = grid[r][c] + max(down_paths, right_paths)
+  return memo[pos]
+
+  
+
   
 
       
