9b107889a064f8b9c9648213886657ca011ec1bf
Mai Vang <vmaineng@gmail.com>
2026-01-30 10:04:29 -0800

    added in ways

--- struct/dec2025.py
+++ struct/dec2025.py
@@ -2790,6 +2790,31 @@ def _summing_squares(n, memo):
   memo[n] = min_squares
   return min_squares
 
+def counting_change(amount, coins):
+  return _counting_change(amount, coins, 0, {})
+
+
+def _counting_change(amount, coins, i, memo):
+  key = (amount, i)
+  if key in memo:
+    return memo[key]
+    
+  if amount == 0:
+    return 1
+
+  if i == len(coins):
+    return 0
+
+  coin = coins[i]
+  total_ways = 0
+  for qty in range(0, (amount // coin) + 1):
+    remainder = amount - (qty * coin)
+    total_ways += _counting_change(remainder, coins, i + 1, memo)
+  memo[key] =total_ways
+  return total_ways
+
+  
+
 
 
   
