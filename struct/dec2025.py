6f1e4b5fdcfc589481448b6b77721b43af43f936
Mai Vang <vmaineng@gmail.com>
2026-01-30 11:04:14 -0800

    added in base case

--- struct/dec2025.py
+++ struct/dec2025.py
@@ -2814,6 +2814,24 @@ def _counting_change(amount, coins, i, memo):
   return total_ways
 
   
+def array_stepper(numbers):
+  return _array_stepper(numbers, 0, {})
+
+def _array_stepper(numbers, i, memo):
+  if i in memo:
+    return memo[i]
+    
+  if i >= len(numbers) -1:
+    return True
+
+  max_step = numbers[i]
+  for step in range(1, max_step + 1):
+    if _array_stepper(numbers, i + step, memo):
+      memo[i] = True
+      return True
+  memo[i] = False
+  return False
+    
 
 
 
