902dab5f6d8436e4087f634927c57378c5e1371a
Mai Vang <vmaineng@gmail.com>
2026-01-30 09:40:59 -0800

    added in squares

--- struct/dec2025.py
+++ struct/dec2025.py
@@ -2769,6 +2769,26 @@ def build_graph(self, numCourses: int,prerequisites: List[List[int]]):
 
     return graph 
 
+import math
+
+def summing_squares(n):
+  return _summing_squares(n, {})
+
+def _summing_squares(n, memo):
+  if n== 0:
+    return 0
+
+  if n in memo:
+    return memo[n]
+
+  min_squares = float('inf')
+  
+  for i in range(1, math.floor(math.sqrt(n)) + 1):
+    square = i * i
+    num_squares = 1 + _summing_squares(n - square, memo)
+    min_squares = min(min_squares, num_squares)
+  memo[n] = min_squares
+  return min_squares
 
 
 
