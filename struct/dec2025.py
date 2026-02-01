742e204c196ac08dc019eee5918b14b7bfcc72b3
Mai Vang <vmaineng@gmail.com>
2026-02-01 10:54:30 -0800

    added in memokey

--- struct/dec2025.py
+++ struct/dec2025.py
@@ -2880,7 +2880,27 @@ def _max_palin_subsequence(string, i , j, memo):
     _max_palin_subsequence(string, i,j -1, memo))
   return memo[key]
 
+def overlap_subsequence(string_1, string_2):
+  return _overlap_subsequence(string_1, string_2, 0, 0, {})
 
+def _overlap_subsequence(string_1, string_2, i, j, memo):
+  #base case: if i or j ever reaches end of string, return count
+  key = (i, j)
+  if key in memo:
+    return memo[key]
+
+  if i == len(string_1) or j == len(string_2):
+    return 0
+
+  if string_1[i] == string_2[j]:
+    memo[key] =  1 + _overlap_subsequence(string_1, string_2, i + 1, j + 1, memo)
+  else:
+   memo[key] =  max(
+      _overlap_subsequence(string_1, string_2, i + 1, j, memo),
+      _overlap_subsequence(string_1, string_2, i, j + 1, memo))
+  return memo[key]
+
+  
 
 
 
