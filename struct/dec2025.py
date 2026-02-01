db1e140fb050f950329c520dabe3a865800f020b
Mai Vang <vmaineng@gmail.com>
2026-02-01 11:06:39 -0800

    madded in algo

--- struct/dec2025.py
+++ struct/dec2025.py
@@ -2901,6 +2901,27 @@ def _overlap_subsequence(string_1, string_2, i, j, memo):
   return memo[key]
 
   
+def can_concat(s, words):
+  return _can_concat(s, words, {})
+
+def _can_concat(s, words, memo):
+  if s in memo:
+    return memo[s]
+    
+  if s == "":
+    return True
+
+  for word in words:
+    if s.startswith(word):
+      suffix = s[len(word):]
+      if _can_concat(suffix, words, memo) == True:
+        memo[s] = True
+        return True
+        
+  memo[s] = False
+  return False
+      
+  
 
 
 
