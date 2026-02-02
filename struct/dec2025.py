9df497b7737785c1270a416bd6a27a35d0c202e1
Mai Vang <vmaineng@gmail.com>
2026-02-02 07:38:27 -0800

    added in algo

--- struct/dec2025.py
+++ struct/dec2025.py
@@ -2940,6 +2940,31 @@ def _can_concat(s, words,i, memo):
   memo[i] = False
   return False
       
+
+      def quickest_concat(s, words):
+  result = _quickest_concat(s, words, {})
+  if result == float('inf'):
+    return -1
+  else:
+    return result
+
+def _quickest_concat(s, words, memo):
+  if s in memo:
+    return memo[s]
+  
+  if s == "":
+    return 0
+
+  min_words = float('inf')
+  for word in words:
+    if s.startswith(word):
+      suffix = s[len(word):]
+      attempt = 1 + _quickest_concat(suffix, words, memo)
+      min_words = min(attempt, min_words)
+  memo[s] = min_words
+  return min_words
+      
+
   
 
 
