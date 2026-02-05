606d438a257890653940c74b712b7e61680d11f5
Mai Vang <vmaineng@gmail.com>
2026-02-05 09:35:54 -0800

    added in algo

--- struct/dec2025.py
+++ struct/dec2025.py
@@ -3315,7 +3315,27 @@ def combine_intervals(intervals):
 
   return -1
 
+def lexical_order(word_1, word_2, alphabet):
+  #receive two string of lowers case chars, and an alphabet
+  #return True if word1 < word_2
+  #ex: 'app', 'apple' => True
 
+
+  #iterate through longest string
+  #obtain each char in the string as long as they are not out of bounds
+  #check the index position of them in alphabet
+  #if word_1 char < word_2 char, return True, else return False
+
+  max_length = max(len(word_1), len(word_2))
+
+  for i in range(0, max_length):
+    value_1 = alphabet.index(word_1[i]) if i < len(word_1) else float('-inf')
+    value_2 = alphabet.index(word_2[i]) if i < len(word_2) else float('-inf')
+    if value_1 < value_2:
+      return True
+    elif value_2 < value_1:
+      return False
+  return True
       
 
     
