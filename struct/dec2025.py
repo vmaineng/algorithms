2359e1f42878ca85bf9288a1a46b811a7d89e945
Mai Vang <vmaineng@gmail.com>
2026-02-09 08:27:14 -0800

    added in dec

--- struct/dec2025.py
+++ struct/dec2025.py
@@ -3504,6 +3504,42 @@ def create_linked_list(values):
 
   return dummy.next
 
+class Node:
+  def __init__(self, val):
+    self.val = val
+    self.next = None
+
+class Queue:
+  def __init__(self):
+    self.head = None
+    self.tail = None
+    self.size = 0
+    
+  def enqueue(self, val):
+    if self.size == 0:
+      self.head = Node(val)
+      self.tail = self.head
+    else:
+      self.tail.next = Node(val)
+      self.tail = self.tail.next
+    self.size += 1
+    
+  def dequeue(self):
+    if self.size == 0:
+      return None
+      
+    value = self.head.val
+    if self.size == 1:
+      self.head = None
+      self.tail = None
+    else:
+      self.head = self.head.next
+      
+    self.size -= 1
+    return value
+    
+      
+  
 
 
     
