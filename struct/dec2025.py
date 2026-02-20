b814c998f67e4ae097cb7e9fc0e20356f8589398
Mai Vang <vmaineng@gmail.com>
2026-02-20 08:48:27 -0800

    added in edge

--- struct/dec2025.py
+++ struct/dec2025.py
@@ -4174,6 +4174,48 @@ def build_graph(rivalries):
 
   return graph
 
+def tolerant_teams(rivalries):
+  #receive a list of names
+  #return boolean
+  #ex:
+
+  #build a graph
+  graph = build_graph(rivalries)
+  coloring ={}
+
+  for node in graph:
+    if node not in coloring:
+      if validate(graph, node, coloring, False) == False:
+        return False
+
+  return True
+
+def validate(graph, node, coloring, current_color):
+  if node in coloring:
+    return coloring[node] == current_color
+
+  coloring[node] = current_color
+
+  for neighbor in graph[node]:
+    if validate(graph, neighbor, coloring, not current_color) == False:
+        return False
+  return True
+    
+
+def build_graph(edges):
+  graph = {}
+
+  for edge in edges:
+    a,b = edge
+    if a not in graph:
+      graph[a] = []
+    if b not in graph:
+      graph[b] = []
+
+    graph[a].append(b)
+    graph[b].append(a)
+
+  return graph
   
   
     
