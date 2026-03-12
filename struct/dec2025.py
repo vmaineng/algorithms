4eb1e0af353b708bd67d6d9a4e071898f0c801a5
Mai Vang <vmaineng@gmail.com>
2026-03-12 09:12:24 -0700

    added in stru

--- struct/dec2025.py
+++ struct/dec2025.py
@@ -5214,3 +5214,25 @@ def _min_change(amount, coins, memo):
     memo[amount] = min_coins
   return min_coins
   
+def count_paths(grid):
+ return _count_paths(grid, 0,0, {})
+
+
+def _count_paths(grid, r, c, memo):
+  pos = (r, c)
+
+  if pos in memo:
+    return memo[pos]
+  
+  if r == len(grid) or c == len(grid[0]) or grid[r][c] == 'X':
+    return 0
+
+  if r == len(grid) -1 and c == len(grid[0]) -1:
+    return 1
+
+  down = _count_paths(grid, r + 1, c, memo)
+  right = _count_paths(grid, r, c +1, memo)
+  
+
+  memo[pos] = down + right
+  return memo[pos]
