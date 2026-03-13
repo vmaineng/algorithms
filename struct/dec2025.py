fbf4c770b5e55ed86c33c98449b6743468480890
Mai Vang <vmaineng@gmail.com>
2026-03-13 08:53:21 -0700

    added in answer

--- struct/dec2025.py
+++ struct/dec2025.py
@@ -5266,3 +5266,40 @@ def _count_paths(grid, r, c, memo):
   memo[pos] = bottom + right
   
   return memo[pos]
+
+def max_path_sum(grid):
+  return _max_path_sum(grid, 0, 0, {})
+  #receive a grid
+  #return the max sum found in the grid
+  #ex: 
+
+
+  #base case:
+  #if we are bottom corner, return the cell value of it
+
+  #add the cell value to each 
+  #if sum > max_sum, update max_summ
+  #return max _ sum
+
+def _max_path_sum(grid, r, c, memo):
+  pos = (r,c)
+  if pos in memo:
+    return memo[pos]
+  
+  if r == len(grid) or c == len(grid[0]):
+    return float('-inf')
+
+
+  if r == len(grid) - 1 and c == len(grid[0])- 1:
+    return grid[r][c]
+    
+
+  bottom = _max_path_sum(grid, r + 1, c, memo)
+  right = _max_path_sum(grid, r, c +1, memo)
+
+  memo[pos] = grid[r][c] + max(bottom, right)
+  return memo[pos]
+
+  
+  
+
