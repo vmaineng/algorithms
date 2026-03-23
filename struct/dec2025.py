d70bad44ca628f3434b0050820cd3c705c306e27
Mai Vang <vmaineng@gmail.com>
2026-03-23 08:37:14 -0700

    added in vals

--- struct/dec2025.py
+++ struct/dec2025.py
@@ -5837,3 +5837,22 @@ def _merge_sort(l1, l2):
   merged += l2
   return merged
     
+    # class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.left = None
+#     self.right = None
+
+def tree_includes(root, target):
+  #receive a root node and a target
+  #return a boolean
+
+  #ex: 
+
+  if root is None:
+    return False
+
+  if root.val == target:
+    return True 
+
+  return tree_includes(root.left, target) or tree_includes(root.right, target)
