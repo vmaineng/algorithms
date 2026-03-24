f7c47156ff9d1f09ceb7202a7ab042ac17b9263a
Mai Vang <vmaineng@gmail.com>
2026-03-24 09:23:49 -0700

    added in num

--- struct/dec2025.py
+++ struct/dec2025.py
@@ -5956,3 +5956,25 @@ def tree_sum(root):
   right_sum = tree_sum(root.right)
 
   return root.val + left_sum + right_sum
+
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.left = None
+#     self.right = None
+
+def tree_value_count(root, target):
+  #receive the root node
+  #return the count of how many times target is seen in tree 
+
+  #ex: 
+
+  #if not == target, return 1
+  #if not root = return 0
+
+  if not root:
+    return 0
+
+  match = 1 if root.val == target else 0
+
+  return match + tree_value_count(root.left, target) + tree_value_count(root.right, target)
