2368d4b1cfd3164c1e4bbfc0c67cefc8b972d174
Mai Vang <vmaineng@gmail.com>
2026-03-27 14:24:39 -0700

    added in notes

---
+++ advent2022/test.py
@@ -0,0 +1,61 @@
+
+#receive a string
+#return middle char
+#ex: 'hello' => 'l'
+
+#iterate with two pointers
+#first one starts at beginning
+#second pointer starts at end
+#keep decrementing down until pointers match each other
+
+
+def findMiddle(str):
+    i = 0
+    j = len(str) - 1
+
+    while i <= j:
+        i += 1
+        j -= 1
+        if len(str) % 2 == 0:
+            return str[i] + str[j]
+        else:
+            return str[i]
+
+
+    # mid = len(str) // 2
+
+    # if len(str) % 2 == 0:
+    #     return str[mid -1] +str[mid]
+    # else:
+    #     return str[mid]
+
+
+# print(findMiddle('hello'))
+# print(findMiddle('race'))
+
+
+
+
+#summation
+#find the summation of every number from 1 to number given
+#number is postive and > 0
+
+
+#receive a postive whole integer > 0
+#return the sum of every number up to number
+#ex: 1 => 1
+#ex: 5 => 5 + 4 + 3 + 2 +1 => 15
+
+#iniitalize a total set to 0
+#iterate up to number given
+#add it all up total
+#return the total
+
+def summation(num):
+    total = 0 # 0 
+    for number in range(0, num + 1): #(0, 1)
+        total += number #0, #1
+    return total #1
+
+print(summation(1))
+print(summation(5))
