3c2af079d9c649b3f4bc324e248eb1864a83419d
Mai Vang <vmaineng@gmail.com>
2026-04-02 09:19:33 -0700

    fizz

--- mar2026/algo.py
+++ mar2026/algo.py
@@ -12,3 +12,28 @@ def array_madness(a,b):
     print(squared_a, squared_b)
     
     return squared_a > squared_b
+
+def fizzbuzz(n):
+    #receive an integer
+    #return a list back
+    #ex: 
+    
+    
+    #initialize an empty list
+    #iterate up to n
+    #check if % 3 and 5 => FizzBuzz
+    #check if 3 => Fizz
+    #if 5, buzz
+    #else add in num 
+    
+    result = []
+    for num in range(1, n + 1):
+        if num % 3 == 0 and num % 5 == 0:
+            result.append('FizzBuzz')
+        elif num % 5 == 0:
+            result.append('Buzz')
+        elif num % 3 == 0: 
+            result.append('Fizz')
+        else:
+            result.append(num)
+    return result
