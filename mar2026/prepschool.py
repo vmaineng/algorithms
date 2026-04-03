04419417a482da157697e6fb31a94b0e1fd6b298
Mai Vang <vmaineng@gmail.com>
2026-04-03 13:49:58 -0700

    added in friends

---
+++ mar2026/prepschool.py
@@ -0,0 +1,27 @@
+#function that filters out string, returns list of friends name in it
+#if name has 4 letters, name is friend otherwise they are not
+
+#receive a list of strings
+#return back a list of our friends whose length is only 4
+#'Mark'
+#receive only alphabetical letters
+#array will never be empty
+
+#ex: ['Mai', 'Mark', 'Amanda'] => ['Mark]
+
+#initialize an empty array that will hold the answer
+#iterate through all of names
+#check the length == 4, add the name to the list
+#return result
+
+#time: O(n) #n 
+#space: O(n)
+
+def findFriends(array):
+    result = []
+    for name in array:
+        if len(name) == 4 or len(name) == 6:
+            result.append(name)
+    return result
+
+print(findFriends(['Mai', 'Mark', 'Amanda']))
