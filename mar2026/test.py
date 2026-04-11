f514503c473e64e76644878c99915ff190513667
Mai Vang <vmaineng@gmail.com>
2026-04-10 18:09:22 -0700

    added in algo

--- mar2026/test.py
+++ mar2026/test.py
@@ -382,3 +382,36 @@ class Solution:
 
 #time:O(n^2)
 #space:O(n^2)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        #receive the head of a linked list
+        #return the linked list back reversed
+
+        #ex: 1 -> 2 -> 3 -> 4
+
+        #ex: 4 -> 3 -> 2 -> 1
+            #             prev    current
+
+        #keep track of the head
+        #initialize an extra pointer
+        #iterate through the entire linked list until it hits null
+        #reverse the pointers and make it point the previous
+        #then we return the current
+
+        prev = None
+        current = head
+
+        while current is not None:
+            nextNode = current.next
+            current.next = prev
+            prev = current
+            current = nextNode
+        return prev
+            
