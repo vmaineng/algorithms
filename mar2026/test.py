aa2d2cdd76eeecb7ac089042ac6c96f677dd1194
Mai Vang <vmaineng@gmail.com>
2026-04-15 08:42:29 -0700

    added in low

--- mar2026/test.py
+++ mar2026/test.py
@@ -517,3 +517,16 @@ def square_root(n):
       high = mid - 1
       leftmost = mid
   return leftmost
+
+  def min_in_rotated_sorted_array(nums):
+  low = 0
+  high = len(nums) - 1
+
+  while low < high:
+    mid = (low + high) // 2
+
+    if nums[mid] < nums[high]:
+      high = mid
+    else:
+      low = mid + 1
+  return nums[low]
