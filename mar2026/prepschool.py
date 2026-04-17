76fa1e06a0e04d33a4435ea9c1e45cefbe2ad778
Mai Vang <vmaineng@gmail.com>
2026-04-17 13:51:20 -0700

    added in prep

--- mar2026/prepschool.py
+++ mar2026/prepschool.py
@@ -32,4 +32,63 @@ def addList(array):
         result.append(f"{idx + 1}")
         result.append(":" + item)
     return result
-print(addList(['a','b','d']))
+# print(addList(['a','b','d']))
+
+
+def findPangram(string):
+    #pangram is a sentence that contains every letter at least once
+    #receive a letter of string
+    #return True or False
+
+    #ex: The quick brown fox jumps over the lazy dog
+    
+    #iterate through sentence
+    #join them all together as one
+    #check if chars exist in my counterChars object where it has one of the chars
+
+    joinSentence = ''.join(string.split()).lower()
+
+    counter = {}
+    for char in joinSentence:
+        counter[char] = counter.get(char, 0) + 1
+
+    charCounter = { 
+        'a': 1,
+        'b':1,
+        'c': 1,
+        'd': 1,
+        'e': 1,
+        'f': 1,
+        'g': 1,
+        'h': 1,
+        'i': 1,
+        'j':1,
+        'k':1,
+        'l': 1,
+        'm': 1,
+        'n': 1,
+        'o': 1,
+        'p': 1,
+        'q': 1,
+        'r':1,
+        's': 1,
+        't': 1,
+        'u': 1,
+        'v': 1,
+        'w':1,
+        'x': 1,
+        'y': 1,
+        'z': 1
+
+    }
+    
+    for char in charCounter:
+        if char not in counter:
+            return False
+    return True
+    
+    
+
+print(findPangram('hello world'))
+print(findPangram('The quick brown fox jumps over the lazy dog'))
+
