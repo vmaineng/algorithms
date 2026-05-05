ca12e0d1b9dd3c22e4b3c2bcd0bfa0ad416d61fd
Mai Vang <vmaineng@gmail.com>
2026-05-05 13:42:45 -0700

    squared

--- mar2026/test.py
+++ mar2026/test.py
@@ -763,3 +763,15 @@ def get_ascii(ch : str) -> int:
     
     return ord(ch)
   
+  import math
+
+def predict_age(age_1, age_2, age_3, age_4, age_5, age_6, age_7, age_8):
+    #receive integers
+    #return the age
+    #ex: 
+    
+    array = [age_1, age_2, age_3, age_4, age_5, age_6, age_7, age_8]
+    total = sum(num * num for num in array)
+    squared = math.sqrt(total)
+    return squared // 2
+    
