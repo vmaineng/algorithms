a41884e54dfcf0056a0090f476f3a139fae4b7e3
Mai Vang <vmaineng@gmail.com>
2026-05-14 10:18:58 -0700

    added in prev

--- mar2026/test.py
+++ mar2026/test.py
@@ -992,3 +992,18 @@ def reverse_list(head):
 
   return prev
 
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.next = None
+
+def reverse_list(head, prev = None):
+  if head is None:
+    return prev
+
+  next = head.next
+  head.next = prev
+
+  return reverse_list(next, head)
+
+
