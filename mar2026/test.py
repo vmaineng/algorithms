e7090aac90872fdd8bf75d59cf86a3848a16e531
Mai Vang <vmaineng@gmail.com>
2026-06-03 11:03:17 -0700

    added in nums

--- mar2026/test.py
+++ mar2026/test.py
@@ -1279,4 +1279,36 @@ class Solution:
         return result
 
 
+
+        class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        #receive two list of integers
+        #return one list back with same amount of integers
+
+        #ex: 
+
+        #keep count
+
+        # result = []
+        # for num in nums1:
+        #     if num in nums2:
+        #         result.append(num)
+        # return result
+
+        #time:O(n^2)
+        #space: O(n)
+
+        seen = {}
+
+        for num in nums1:
+            seen[num] = seen.get(num, 0) + 1
+        
+        result = []
+        for num in nums2:
+            if seen.get(num,0) > 0:
+                result.append(num)
+                seen[num] -= 1
+        return result
+
+
         
