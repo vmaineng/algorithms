170a8c00984e24eedf02b986c5adef8522a8836b
Mai Vang <vmaineng@gmail.com>
2026-06-03 17:19:26 -0700

    added in algo

--- mar2026/algo.py
+++ mar2026/algo.py
@@ -276,4 +276,31 @@ def pair_sum(numbers, target_sum):
   result = [num for num in b if num in uniqueA]
   return result
     
-  
+  import React from "react"
+import pads from "./pads"
+
+
+export default function App() {
+    const [statePads, setStatePads] = React.useState(pads)
+    /**
+     * Challenge part 1:
+     * 1. Initialize state with the default value of the
+     *    array pulled in from pads.js
+     * 2. Map over that state array and display each one
+     *    as a <button> (CSS is already written for you)
+     *    (Don't worry about using the "on" or "color" 
+     *    properties yet)
+     */
+    return (
+        <main>
+            <div className="pad-container">
+          {statePads.map((pad) => (
+                    <button key={pad.id}>
+                        Click me
+                    </button>
+                ))}
+            </div>
+        </main>
+    )
+}
+
