0ec4d6c8ff627d2aec7d23e2977dede83c979609
Mai Vang <vmaineng@gmail.com>
2026-06-09 10:27:51 -0700

    added in graph

--- mar2026/test.py
+++ mar2026/test.py
@@ -1417,6 +1417,49 @@ def closest_carrot(grid, starting_row, starting_col):
 
   return -1
     
+    from collections import deque
+
+def shortest_path(edges, node_A, node_B):
+  #receive an edge list
+
+  #return shortest shortest_path
+  #ex:
+
+
+  #build graph
+  #travel form w to z
+  #increment dst
+  #return dst
+
+
+  graph = build_graph(edges)
+  visited = set()
+  queue = deque([(node_A, 0)])
+
+  while queue:
+    current, dst = queue.popleft()
+
+    if current == node_B:
+      return dst
+
+    for neighbor in graph[current]:
+      if neighbor not in visited:
+        queue.append((neighbor, dst + 1))
+        visited.add(neighbor)
+  return -1
+
+def build_graph(edges):
+  graph = {}
+
+  for a,b in edges:
+    if a not in graph:
+      graph[a] = []
+    if b not in graph:
+      graph[b] = []
+    graph[a].append(b)
+    graph[b].append(a)
+  return graph
+  
 
 
         
