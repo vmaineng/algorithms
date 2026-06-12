cd5a8ae492ebc44a99281eafac1256110695b36b
Mai Vang <vmaineng@gmail.com>
2026-06-12 12:45:51 -0700

    added in algo

--- mar2026/test.py
+++ mar2026/test.py
@@ -1628,3 +1628,42 @@ def build_graph(n, roads):
     
   return graph
   
+  def safe_cracking(hints):
+  #build graph
+  #topological order
+
+  graph = build_graph(hints)
+  return topological_order(graph)
+
+def build_graph(hints):
+  graph = {}
+
+  for a,b in hints:
+    if a not in graph:
+      graph[a] = []
+    if b not in graph:
+      graph[b] = []
+    graph[a].append(b)
+  return graph
+
+def topological_order(graph):
+  num_parents = {}
+  for node in graph:
+    num_parents[node] = 0
+
+  for node in graph:
+    for child in graph[node]:
+      num_parents[child] += 1
+
+  ready = [node for node in graph if num_parents[node] == 0]
+
+  order = ''
+  while ready:
+    node = ready.pop()
+    order += str(node)
+    for child in graph[node]:
+      num_parents[child] -=1
+      if num_parents[child] == 0:
+        ready.append(child)
+
+  return order
