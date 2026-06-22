e14040238736c3a8350aab3242c9e3135ff84757
Mai Vang <vmaineng@gmail.com>
2026-06-21 17:57:35 -0700

    added in profi

--- mar2026/test.py
+++ mar2026/test.py
@@ -2130,4 +2130,13 @@ class Solution:
                 nums[i] = nums[j]
         return i + 1
         
-  
+  class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        #keep track of total 
+
+        max_profit = 0
+
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                max_profit += prices[i] - prices[i - 1]
+        return max_profit
