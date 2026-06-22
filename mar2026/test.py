b42e6263c31941b65b64499667d50753b868c9a0
Mai Vang <vmaineng@gmail.com>
2026-06-22 09:17:15 -0700

    added in test

--- mar2026/test.py
+++ mar2026/test.py
@@ -2209,3 +2209,19 @@ def tree_sum(root):
 
   return root.val + left + right
 
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.left = None
+#     self.right = None
+
+def tree_includes(root, target):
+
+  if not root:
+    return False 
+    
+  if root.val == target:
+    return True 
+
+  return tree_includes(root.left, target) or tree_includes(root.right, target)
+
