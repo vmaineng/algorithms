9b5c2ea8e040a5219745241f5c47585a1b2d01cf
Mai Vang <vmaineng@gmail.com>
2026-06-23 08:33:39 -0700

    added in left

--- mar2026/test.py
+++ mar2026/test.py
@@ -2363,8 +2363,35 @@ class Solution:
         return total
 
 
-        
+      # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 
+
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        #receive the root node
+        #return an integer of count of nodes in trees
+        #ex: 
+
+        #using bfs
+
+        if not root:
+            return 0
+
+        if not root.left and not root.right:
+            return 1
+
+        left = self.countNodes(root.left)
+        right = self.countNodes(root.right)
+
+        return 1 + left + right
+
+
+        
   
 
 
