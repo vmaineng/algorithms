08f4249701aaff14faf55bfd895e9d51c062a6ea
Mai Vang <vmaineng@gmail.com>
2026-06-24 08:39:51 -0700

    added in path

--- mar2026/test.py
+++ mar2026/test.py
@@ -2390,6 +2390,43 @@ class Solution:
 
         return 1 + left + right
 
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.left = None
+#     self.right = None
+
+
+def path_finder(root, target):
+  result = _path_finder(root, target)
+  if result is None:
+    return None
+  else:
+    return result[::-1]
+
+def _path_finder(root, target):
+  #receive the root Node
+  #return a list back of nodes to reach target
+
+  if not root:
+    return None
+
+  if root.val == target:
+    return [ root.val ]
+
+  left = _path_finder(root.left, target)
+  if left is not None:
+    left.append(root.val)
+    return left
+  right = _path_finder(root.right, target)
+  if right is not None:
+    right.append(root.val)
+    return right
+  return None
+
+
+
+  
 
         
   
