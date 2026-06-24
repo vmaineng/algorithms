70f00c15258f2d13fba48802a21eabea1811fde4
Mai Vang <vmaineng@gmail.com>
2026-06-24 09:07:41 -0700

    added in root

--- mar2026/test.py
+++ mar2026/test.py
@@ -2457,6 +2457,24 @@ def how_high(root):
 
   return 1 + max(how_high(root.left), how_high(root.right))
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        def dfs(node, current):
+            if not node:
+                return 0
+            
+            current = current * 10 + node.val
+            if not node.left and not node.right:
+                return current
+            return dfs(node.left, current) + dfs(node.right, current)
+        return dfs(root, 0)
+
 
         
   
