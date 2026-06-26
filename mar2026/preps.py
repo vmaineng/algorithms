25c48a4708a2a66831c5ec1b08d89d4b2229d8d8
Mai Vang <vmaineng@gmail.com>
2026-06-26 13:45:29 -0700

    added in friend

---
+++ mar2026/preps.py
@@ -0,0 +1,31 @@
+#friend or foe
+#filters a list of strings
+
+#if name has 4 letters == friend, else foe
+
+#input = a list of strings
+#output = a list of strings that are friends that has a length of 4
+#ex: ['Mai', 'Kyle', 'Eric', 'Amanda'] => [ 'Kyle', 'Eric' ]
+
+#[] => []
+#[34, ] => always be strings only
+#['Mai']
+#with names longer than 4=> []
+
+#initalize an empty list
+#iterate through the input string
+#check the length of name == 4
+#add it to our empty list
+#return the result list of names == 4
+
+def friendOrFoe(array):
+    result = []
+    for name in array:
+        if len(name) == 4:
+            result.append(name)
+    return result
+
+print(friendOrFoe(['Mai', 'Kyle', 'Eric', 'Amanda']))
+print(friendOrFoe(['Mai', 'Kye', 'Ric', 'Amanda']))
+
+
