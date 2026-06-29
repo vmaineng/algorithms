30eaf7f833d4363411f95fbee021eb2746b4f7e8
Mai Vang <vmaineng@gmail.com>
2026-06-29 09:26:13 -0700

    aded in bin

--- mar2026/test.py
+++ mar2026/test.py
@@ -2882,6 +2882,14 @@ def middle_value(head):
 
   return slow.val
 
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        int_a = int(a, 2)
+        int_b = int(b,2)
+        total = int_a + int_b
+        return bin(total)[2:]
+        
+
 
   
     
