7963d357d066e2737ccf8ea18a766fc21990d6f4
Mai Vang <vmaineng@gmail.com>
2026-07-10 09:31:09 -0700

    coomit nums

--- mar2026/test.py
+++ mar2026/test.py
@@ -3439,7 +3439,25 @@ def get_matrix(n):
     return result
     
     
-    
+    import heapq
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        # sortNums = sorted(nums)
+
+        # return sortNums[-k]
+
+
+        heap = []
+        for num in nums:
+            heapq.heappush(heap, num)
+
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        return heap[0]
+
+
 
     
   
