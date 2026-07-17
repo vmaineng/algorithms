61e3f651ce7517d52326189af9d3d853f750f8ef
Mai Vang <vmaineng@gmail.com>
2026-07-17 07:11:22 -0700

    added

--- mar2026/test.py
+++ mar2026/test.py
@@ -3457,6 +3457,44 @@ class Solution:
 
         return heap[0]
 
+class NumMatrix:
+
+    def __init__(self, matrix: List[List[int]]):
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        self.prefix = [[0] * (cols + 1) for _ in range(rows + 1)]
+
+        for r in range(rows):
+            for c in range(cols):
+                self.prefix[r + 1][c + 1] = (
+                    self.prefix[r][c + 1]
+                    + self.prefix[r + 1][c]
+                    - self.prefix[r][c]
+                    + matrix[r][c]
+                )
+        
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        return (
+            self.prefix[row2 + 1][col2 + 1]
+            - self.prefix[row1][col2 + 1]
+            - self.prefix[row2 + 1][col1]
+            + self.prefix[row1][col1]
+        )
+
+#         everything above
+# +
+# everything left
+# -
+# overlap
+# +
+# current value
+
+
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
 
 
     
