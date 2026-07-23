3eea1c42780da45a1df30aa9e15754d20ab6a5ae
Mai Vang <vmaineng@gmail.com>
2026-07-23 08:04:00 -0700

    added inf ast

--- mar2026/test.py
+++ mar2026/test.py
@@ -3689,7 +3689,24 @@ class Solution:
                         return False
         return len(stack) == 0
         
-        
+        # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+        return False
   
 
 
