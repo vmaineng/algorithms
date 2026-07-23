c855a44633940dbea59508a6911bea0c4e840ec3
Mai Vang <vmaineng@gmail.com>
2026-07-23 08:18:22 -0700

    added in result

--- mar2026/test.py
+++ mar2026/test.py
@@ -3709,4 +3709,33 @@ class Solution:
         return False
   
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        #receive root of binnary tree
+        #return the nodes swapped
+        #ex: 
+        if not root:
+            return None
+
+       
+        queue = deque([root])
+
+        while queue: 
+            current = queue.popleft()
+            current.left, current.right = current.right, current.left
+            
+            if current.left:
+                queue.append(current.left)
+
+            if current.right:
+                queue.append(current.right)
+            
+        return root
 
