23a45760de7ede096cfd4ac6a0fac0c684f0d0ab
Mai Vang <vmaineng@gmail.com>
2026-07-31 09:07:22 -0700

    added in 250

--- july2027/neet.py
+++ july2027/neet.py
@@ -256,3 +256,25 @@ class Solution:
         while len(tmp):
             self.stack.append(tmp.pop())
         return mini
+
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
