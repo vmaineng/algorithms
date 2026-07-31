e29ed862092a02c265327ce60e75f6a2f1fcb4b3
Mai Vang <vmaineng@gmail.com>
2026-07-31 14:12:19 -0700

    added in prep

--- july2027/prep.py
+++ july2027/prep.py
@@ -9,4 +9,16 @@ def charAccum(str):
         
     return '-'.join(result)
 
-print(charAccum('abc'))
+# print(charAccum('abc'))
+
+
+def weighsMore(str1,str2):
+    #take the first num // second num
+    #see which one out weighs the other
+    #ex: ('1:3','1:2')
+
+    totalOne = int(str1[0]) // int(str1[2])
+    totalTwo = int(str2[0]) // int(str2[2])
+
+    return True if totalOne > totalTwo else False
+print(weighsMore('1:3','1:2'))
