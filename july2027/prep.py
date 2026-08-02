34fe540349b6d86185ea8b1b91cab498fb424d76
Mai Vang <vmaineng@gmail.com>
2026-08-01 18:55:56 -0700

    added in post

--- july2027/prep.py
+++ july2027/prep.py
@@ -22,3 +22,39 @@ def weighsMore(str1,str2):
 
     return True if totalOne > totalTwo else False
 print(weighsMore('1:3','1:2'))
+
+class Dog ():
+  def __init__(self, breed):
+    self.breed = breed
+    
+
+snoopy = Dog("Beagle")
+
+snoopy.bark = lambda: "Woof"
+
+scoobydoo = Dog("Great Dane")
+scoobydoo.bark = lambda: "Woof"
+
+import heapq
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        #receive a large unsorted nums array
+        #return the kth largest element
+        #ex: [3,2,5,1] => 2 
+        # [1,2,3,5] => 2 # time: O(n log n); #space: O(n)
+
+        #initialize a max heap
+        #iterate through the integers
+        #add all of the integers until it reaches the length of k
+        #then we are oging to opop it off until we hit k times
+
+        heap = [ ]
+
+        for num in nums:
+            heapq.heappush(heap, num) 
+            if len(heap) >k:
+                heapq.heappop(heap)
+
+        
+        return heap[0] #O(logK) #space:O(K)
