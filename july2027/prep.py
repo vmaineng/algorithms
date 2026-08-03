c25dfd4cb2e835abe4c0b19fdabd7fda35422826
Mai Vang <vmaineng@gmail.com>
2026-08-03 16:37:52 -0700

    added in pre

--- july2027/prep.py
+++ july2027/prep.py
@@ -311,3 +311,22 @@ class Solution:
         if head2:
             current.next = head2
         return dummy.next
+
+    # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        def preorder(node):
+            if not node:
+                return
+            
+            result.append(node.val)
+            preorder(node.left)
+            preorder(node.right)
+        preorder(root)
+        return result
