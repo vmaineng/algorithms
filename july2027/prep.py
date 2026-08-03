116002d11271f4b2fe120dd47a2484b45d9abbb6
Mai Vang <vmaineng@gmail.com>
2026-08-03 16:38:53 -0700

    added in post

--- july2027/prep.py
+++ july2027/prep.py
@@ -330,3 +330,23 @@ class Solution:
             preorder(node.right)
         preorder(root)
         return result
+
+    # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+
+        def postorder(node):
+            if not node:
+                return
+
+            postorder(node.left)
+            postorder(node.right)
+            result.append(node.val)
+        postorder(root)
+        return result
