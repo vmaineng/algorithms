da4b4643980413e6caa6a121b313abab64efd6e8
Mai Vang <vmaineng@gmail.com>
2026-08-03 16:17:25 -0700

    added in mid

--- july2027/prep.py
+++ july2027/prep.py
@@ -236,3 +236,23 @@ class Solution:
                 buy = sell
         return maxprofit
 
+    class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        #receive a list of integers
+        #return the idx position of the target using binary search
+
+
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif target > nums[mid]:
+                left = mid + 1
+            else:
+                right = mid -1
+        return -1
+
