b713920f1e7d0d9a0e8c851e93bab9d98fc77b71
Mai Vang <vmaineng@gmail.com>
2026-08-03 15:31:25 -0700

    added in seen

--- july2027/prep.py
+++ july2027/prep.py
@@ -186,4 +186,19 @@ class Solution:
             else:
                 return False
         return True
-        
+
+
+        class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        seen = set()
+
+        left = 0
+
+        for right in range(len(nums)):
+            if right - left > k:
+                seen.remove(nums[left])
+                left += 1
+            if nums[right] in seen:
+                return True
+            seen.add(nums[right])
+        return False
