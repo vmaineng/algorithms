31a5df7edc8b0fdee82c3c2f68b9e3e05e6c261b
Mai Vang <vmaineng@gmail.com>
2026-08-05 09:47:26 -0700

    added in py

--- july2027/prep.py
+++ july2027/prep.py
@@ -435,3 +435,20 @@ class Solution:
                 sell +=1
         return maxProfit
 
+    class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.heap, self.k = nums, k
+        heapq.heapify(self.heap)
+        while len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.heap, val)
+        while len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+        return self.heap[0]
+
+        
+
+
