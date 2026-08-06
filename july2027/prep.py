49baea2e78c6a786f0b036177c3f5784caf6738c
Mai Vang <vmaineng@gmail.com>
2026-08-06 16:22:20 -0700

    added in nums

--- july2027/prep.py
+++ july2027/prep.py
@@ -604,3 +604,11 @@ class MinStack:
         return mini
 
 
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i = 0
+        for j in range(len(nums)):
+            if nums[j] != val:
+                nums[i] = nums[j]
+                i += 1
+        return i
