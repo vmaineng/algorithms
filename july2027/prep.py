eeaecbd7dbf4b88130c9b09f967e5056d5244e09
Mai Vang <vmaineng@gmail.com>
2026-08-06 13:05:12 -0700

    added int mp

--- july2027/prep.py
+++ july2027/prep.py
@@ -577,5 +577,30 @@ class Solution:
                     return word[:i]
         return strs[0]
         
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        tmp = []
+        mini = self.stack[-1]
+
+        while len(self.stack):
+            mini = min(mini, self.stack[-1])
+            tmp.append(self.stack.pop())
+        
+        while len(tmp):
+            self.stack.append(tmp.pop())
+        return mini
 
 
