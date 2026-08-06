63f0f86c09032cb522211def7f5433c71519a4ce
Mai Vang <vmaineng@gmail.com>
2026-08-05 17:23:47 -0700

    added in com

--- july2027/prep.py
+++ july2027/prep.py
@@ -504,6 +504,28 @@ class Solution:
                     return False
         return len(stack) == 0
 
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        five = 0
+        ten = 0
+
+        for bill in bills:
+            if bill == 5:
+                five += 1
+            elif bill == 10:
+                if five == 0:
+                    return False
+                five -=1
+                ten +=1
+            else:
+                if five > 0 and ten > 0:
+                    ten -= 1
+                    five -= 1
+                elif five >= 3:
+                    five -= 3
+                else:
+                    return False
+        return True
 
         
         
