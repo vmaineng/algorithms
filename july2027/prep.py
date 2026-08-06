5c93f9e013078341b8d79f242db348f14cef22b9
Mai Vang <vmaineng@gmail.com>
2026-08-06 13:00:53 -0700

    added in com

--- july2027/prep.py
+++ july2027/prep.py
@@ -568,7 +568,14 @@ class Solution:
             else:
                 seen[nums[i]] = i
     
-        
+
+        class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        for i in range(len(strs[0])):
+            for word in strs:
+                if i == len(word) or word[i] != strs[0][i]:
+                    return word[:i]
+        return strs[0]
         
 
 
