8559feb2b77c1d1ea23afec2016aefa249d288d2
Mai Vang <vmaineng@gmail.com>
2026-08-06 17:59:59 -0700

    added in dfs

--- july2027/prep.py
+++ july2027/prep.py
@@ -693,3 +693,25 @@ class Solution:
                 seen.remove(nums[left])
                 left += 1
         return False
+
+    class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        visited = set()
+
+        def dfs(row, col):
+            if row < 0 or row >= len(grid) or col < 0 or col >= len(grid[0]) or grid[row][col] == 0:
+                return 1
+            if (row, col) in visited:
+                return 0
+
+            visited.add((row, col))
+            
+            size = dfs(row + 1, col) + dfs(row -1, col) + dfs(row, col + 1) + dfs(row, col -1)
+            return size
+
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col]:
+                    return dfs(row, col)
+
+        return 0
