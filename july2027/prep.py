e80057a7f56a005d039d0202af905f751710b507
Mai Vang <vmaineng@gmail.com>
2026-08-06 17:14:46 -0700

    added in heap

--- july2027/prep.py
+++ july2027/prep.py
@@ -622,5 +622,22 @@ class KthLargest:
         self.nums.append(val)
         self.nums.sort()
         return self.nums[len(self.nums)-self.k]
+
+    class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.minHeap = nums
+        heapq.heapify(self.minHeap)
+
+        while len(self.minHeap) > k:
+            heapq.heappop(self.minHeap)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.minHeap, val)
+        while len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+        return self.minHeap[0]
+
         
         
