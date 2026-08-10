7ce14844a5f1a9fe88dd27b5a7254127190977af
Mai Vang <vmaineng@gmail.com>
2026-08-10 08:51:51 -0700

    added in commit

--- july2027/prep.py
+++ july2027/prep.py
@@ -901,4 +901,18 @@ class Solution:
             right -=1
         return True
 
-            
+
+            class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        seen = set()
+        left = 0
+
+        for right in range(len(nums)):
+            if nums[right] in seen:
+                return True
+            seen.add(nums[right])
+
+            if right - left >= k:
+                seen.remove(nums[left])
+                left += 1
+        return False
