f83f3c0077ac319b0fdeed65e6427a7aff461d94
Mai Vang <vmaineng@gmail.com>
2026-08-10 08:48:37 -0700

    added in git

--- july2027/prep.py
+++ july2027/prep.py
@@ -878,3 +878,27 @@ def merge_arrays(first, second):
 
     result = first + second
     return sorted(set(result))
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        #receive a string with chars
+        #return boolean if valid palindrome
+
+        #ex: 
+
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+
+            if s[left].lower() != s[right].lower():
+                return False
+            left += 1
+            right -=1
+        return True
+
+            
