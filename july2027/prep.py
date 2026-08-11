159f3aca612925fbff672ad94d3e71b9fd2e104f
Mai Vang <vmaineng@gmail.com>
2026-08-11 06:29:12 -0700

    added in easy

--- july2027/prep.py
+++ july2027/prep.py
@@ -976,3 +976,20 @@ class Solution:
                 seen.remove(nums[left])
                 left +=1
         return False
+
+    class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s) - 1
+
+        while left <= right: 
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -=1
+
+            if s[left].lower() != s[right].lower():
+                return False
+            left += 1
+            right -=1 
+        return True
