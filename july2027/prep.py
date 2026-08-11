81663890ada4a68e589838c2dd361d9dc5d60d9b
Mai Vang <vmaineng@gmail.com>
2026-08-11 06:26:00 -0700

    added in range

--- july2027/prep.py
+++ july2027/prep.py
@@ -963,3 +963,16 @@ class Solution:
                 return False
         return True
 
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        left = 0
+        seen = set()
+        for right in range(len(nums)):
+            if nums[right] in seen:
+                return True
+            seen.add(nums[right])
+
+            if right - left >= k:
+                seen.remove(nums[left])
+                left +=1
+        return False
