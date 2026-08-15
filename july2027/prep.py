49425acb3d232ecd93a85453dd28bb5d841ea31d
Mai Vang <vmaineng@gmail.com>
2026-08-15 08:25:42 -0700

    added in strs

--- july2027/prep.py
+++ july2027/prep.py
@@ -1163,3 +1163,14 @@ def count_components(n, edges):
     if roots[i] == i:
       count += 1
   return count
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        
+        for i in range(len(strs[0])):
+            for word in strs:
+                if i > len(word) or word[i] != strs[0][i]:
+                    return word[:i]
+        return strs[0]
+                
