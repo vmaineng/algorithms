2328a91e2ca66babe37fc41dba40350f442ba0e0
Mai Vang <vmaineng@gmail.com>
2026-08-20 08:30:37 -0700

    added in fal

--- july2027/prep.py
+++ july2027/prep.py
@@ -1458,6 +1458,40 @@ class Solution:
                 maxProfit += prices[i] -prices[i - 1]
         return maxProfit
         
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        for row in range(9):
+            seen = set()
+            for i in range(9):
+                if board[row][i] == '.':
+                    continue
+                if board[row][i] in seen:
+                    return False
+                seen.add(board[row][i])
 
+        for col in range(9):
+            seen = set()
+            for i in range(9):
+                if board[i][col] == '.':
+                    continue
+                if board[i][col] in seen:
+                    return False
+                seen.add(board[i][col])
+        for square in range(9):
+            seen = set()
+            for i in range(3):
+                for j in range(3):
+                    row = (square//3) * 3 + i
+                    col = (square //3) * 3 + j
+                    if board[row][col] == '.':
+                        continue
+                    if board[row][col] in seen:
+                        return False
+                    seen.add(board[row][col])
+        return True
+                
+
+
+        
 
         
