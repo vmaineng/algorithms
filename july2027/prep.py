2e5e1fc4765ed3c2a8fa3d9485032550c43949ea
Mai Vang <vmaineng@gmail.com>
2026-08-22 16:55:22 -0700

    added in graph

--- july2027/prep.py
+++ july2027/prep.py
@@ -1582,7 +1582,32 @@ def has_path(graph, node, visited):
       
   return True
 
-  
+def largest_component(graph):
+  visited = set()
+  count = 0
+  largestCount = 0
+
+  for node in graph:
+    count = explore(graph, node, visited)
+
+    if count > largestCount:
+      largestCount = count
+
+  return largestCount
+
+
+def explore(graph, node, visited):
+  if node in visited:
+    return 0
+
+  visited.add(node)
+
+  size = 1
+
+  for neighbor in graph[node]:
+    size += explore(graph, neighbor, visited)
+
+  return size  
     
 
         
