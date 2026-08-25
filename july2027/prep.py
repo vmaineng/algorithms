14150cd4247c263a2ac6f5615def129f12846f1a
Mai Vang <vmaineng@gmail.com>
2026-08-25 06:29:54 -0700

    added in height

--- july2027/prep.py
+++ july2027/prep.py
@@ -1667,7 +1667,34 @@ class Solution:
                 idx += 1
 
         
-  
+  # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+
+        leftHeight = self.getHeight(root.left)
+        rightHeight = self.getHeight(root.right)
+
+        if abs(leftHeight - rightHeight) > 1:
+            return False
+
+        return self.isBalanced(root.left) and self.isBalanced(root.right)
+
+    def getHeight(self, node: Optional[TreeNode]) -> bool:
+        if not node:
+            return True
+        
+        leftHeight = self.getHeight(node.left)
+        rightHeight = self.getHeight(node.right)
+
+        return 1 + max(leftHeight, rightHeight)
     
 
         
