e24e5908b39cba30a7d715d4bfa6134de96545a0
Mai Vang <vmaineng@gmail.com>
2026-08-28 12:28:18 -0700

    added in scrim

---
+++ july2027/debug.py
@@ -0,0 +1,7 @@
+def add_task(task):
+    task_list=[]
+    task_list.append(task)
+    return task_list
+
+print(add_task("buy milk"))
+print(add_task("walk dog"))
--- july2027/scrimba.js
+++ july2027/scrimba.js
@@ -46,3 +46,31 @@ export default function Joke(props) {
     )
 }
 
+const menu = [
+    { name: "Margherita", price: 8 },
+    { name: "Pepperoni", price: 10 },
+    { name: "Hawaiian", price: 10 },
+    { name: "Veggie", price: 9 },
+]
+
+const cashInRegister = 100
+const orderQueue = []
+
+/**
+ * Challenge: Add a utility function "addNewPizza" that takes a pizza object
+ * and adds it to the menu.
+ */
+
+
+function addNewPizza(pizzaObj) { 
+    menu.push(pizzaObj)
+
+}
+
+function placeOrder(pizzaName) {
+    const pizzaName = menu.find(pizzaObj => pizzaName === pizzaObj)
+    cashInRegister += pizzaName.price
+    orderQueue.push(pizzaName.name)
+}
+
+
