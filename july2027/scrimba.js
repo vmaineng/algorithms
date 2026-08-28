20ea77ff6f3e9becf1ef43f3961e29d29890f920
Mai Vang <vmaineng@gmail.com>
2026-08-28 15:13:05 -0700

    added in git

--- july2027/scrimba.js
+++ july2027/scrimba.js
@@ -80,3 +80,11 @@ function completeOrder(orderId) {
     order.status = 'completed'
     return order
 }
+
+
+let myName: string = "Bob"
+// Primitive Data Types: string, number, boolean
+// Challenge: Explicitly type the variables below:
+
+let numberOfWheels: number = 4
+let isStudent: boolean = false
---
+++ july2027/typescript.ts
@@ -0,0 +1,80 @@
+// Challenge: try to figure out how to move the nested address object type
+// into a separate type definition. When done correctly, there should be no more
+// red errors in the editor
+
+type Address = {
+     street: string
+        city: string
+        country: string
+    
+}
+
+type Person = {
+    name: string
+    age: number
+    isStudent: boolean
+    address: Address
+}
+
+let person1: Person = {
+    name: "Joe",
+    age: 42,
+    isStudent: true,
+    address: {
+        street: "123 Main",
+        city: "Anytown",
+        country: "USA"
+    }
+}
+
+let person2: Person = {
+    name: "Jill",
+    age: 66,
+    isStudent: false,
+    address: {
+        street: "123 Main",
+        city: "Anytown",
+        country: "USA"
+    }
+}
+
+let people = [person1, person2]
+
+type Order = 'ordered' | 'completed'
+
+type identifier = string | number
+
+function getPizzaDetail(identifier:identifier) {
+    if (identifier === 'string') {
+        menubar.find(getPizzaDetail.name.toLowerCase())
+    } else if (identifier === 'number') { 
+        menubar.find(pizza => pizza.id === identifier)
+    }
+}
+
+type User = {
+    id: number
+    username: string
+    role: "member" | "contributor" | "admin"
+}
+
+const users: User[] = [
+    { id: 1, username: "john_doe", role: "member" },
+    { id: 2, username: "jane_smith", role: "contributor" },
+    { id: 3, username: "alice_jones", role: "admin" },
+    { id: 4, username: "charlie_brown", role: "member" },
+];
+
+function updateUser(id: number, updates: any) {
+    // Find the user in the array by the id
+    // Use Object.assign to update the found user in place. 
+    // Check MDN if you need help with using Object.assign
+
+    users.find(user=> user.id === id)
+}
+
+// Example updates:
+updateUser(1, { username: "new_john_doe" });
+updateUser(4, { role: "contributor" });
+
+console.log(users)
