cabdf9e14fac720cd7c338f5197eebcc2bbc5593
Mai Vang <vmaineng@gmail.com>
2026-09-03 13:06:43 -0700

    added in gethei

--- july2027/run.py
+++ july2027/run.py
@@ -212,3 +212,36 @@ class Solution:
                 left = mid + 1
         
                 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        #receive a binary tree node
+        #return binary tree's height true or false
+
+        return self.getHeight(root) != -1
+
+        
+    def getHeight(self, node):
+        if not node:
+            return 0
+        leftHeight = self.getHeight(node.left)
+
+        if leftHeight == -1:
+            return -1
+
+        rightHeight = self.getHeight(node.right)
+
+        if rightHeight == -1:
+            return -1
+
+        if abs(leftHeight - rightHeight) > 1:
+            return -1
+        return 1 + max(leftHeight, rightHeight)
+
+        
