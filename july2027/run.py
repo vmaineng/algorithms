a8d4f23e825a7670677b24bb0151ae163f491f6b
Mai Vang <vmaineng@gmail.com>
2026-09-04 08:28:37 -0700

    added in subtree

--- july2027/run.py
+++ july2027/run.py
@@ -319,4 +319,35 @@ class Solution:
         self.diameter = max(self.diameter, leftHeight + rightHeight)
 
         return 1 + max(leftHeight, rightHeight)
+
+
+        # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:   
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        #if you get a root node and a subroot node
+        #return if subtree is part of a node
+        #ex:
+
+        if not subRoot:
+            return True
         
+        if not root:
+            return False
+
+        if self.sameTree(root, subRoot):
+            return True
+        return (self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot))
+
+    def sameTree(self, root, subRoot):
+        if not root and not subRoot:
+            return True
+        if root and subRoot and root.val == subRoot.val:
+            return self.sameTree(root.left, subRoot.left) and self.sameTree(root.right, subRoot.right)
+        else:
+            return False
