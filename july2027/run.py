189c1bdf0298510b1bf66c70f5e2d3521df22848
Mai Vang <vmaineng@gmail.com>
2026-09-11 09:18:17 -0700

    added in run

--- july2027/run.py
+++ july2027/run.py
@@ -544,5 +544,12 @@ class Solution:
         return max(globMax, total - globMin) if globMax > 0 else globMax
 
 
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        goal = len(nums) - 1
 
+        for i in range(len(nums)-2, -1, -1):
+            if i + nums[i] >= goal:
+                goal =i
+        return True if goal == 0 else False
         
