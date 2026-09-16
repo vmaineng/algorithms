2d4dfc061d265fcfedd5fd21f2007b298d49fac7
Mai Vang <vmaineng@gmail.com>
2026-09-16 09:50:35 -0700

    adde din point

--- sept2027/algo.py
+++ sept2027/algo.py
@@ -175,4 +175,36 @@ class Solution:
             self.dfs(grid, row, col - 1, visited)
         )
 
+        class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
         
+        #receive a list of integers
+        #return the list sorted without using nums.sort()
+
+        #ex:
+
+        #create two pointers
+        #find max
+        #position it at the far right
+        #increment down
+
+        left = 0
+        mid = 0
+        right = len(nums) - 1
+
+        while mid <= right:
+            if nums[mid] == 0:
+                nums[left], nums[mid] = nums[mid], nums[left]
+                left += 1
+                mid += 1
+
+            elif nums[mid] == 2:
+                nums[right], nums[mid] = nums[mid], nums[right]
+                right -=1
+            else:
+                mid += 1
+                
+ 
