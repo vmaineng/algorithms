f0b8dfc5aa3a108d0c611efe2975283498c49f1a
Mai Vang <vmaineng@gmail.com>
2026-09-19 11:44:28 -0700

    added in interval

--- sept2027/prep.py
+++ sept2027/prep.py
@@ -79,3 +79,25 @@ class Solution:
 
         return -stones[0] if len(stones) > 0 else 0
 
+    """
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        intervals.sort(key=lambda x:x.start)
+
+
+        for i in range(1, len(intervals)):
+            prev = intervals[i - 1]
+            curr = intervals[i]
+            if prev.end > curr.start:
+                return False
+        return True
+
+
+
