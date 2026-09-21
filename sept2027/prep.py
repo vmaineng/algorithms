39ad1d12e799572a91089a80ed91c4bd815aa2fa
Mai Vang <vmaineng@gmail.com>
2026-09-21 07:55:37 -0700

    added in interval

--- sept2027/prep.py
+++ sept2027/prep.py
@@ -179,3 +179,27 @@ def highest_rank(arr):
             max_count = val
             max_val = max(key, max_val)
     return max_val
+
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        #receive a class of in list of (start, end)
+        #return boolean
+        #ex: 
+
+        intervals.sort(key=lambda x:x.start)
+
+        for i in range(1, len(intervals)):
+            curr = intervals[i]
+            prev = intervals[i- 1]
+            if prev.end > curr.start:
+                return False
+        return True
+
