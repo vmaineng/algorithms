1b1a2c58ea8151e2c4c368241fab7c7b1dd1971d
Mai Vang <vmaineng@gmail.com>
2026-09-22 08:37:17 -0700

    added in newinterval

--- sept2027/prep.py
+++ sept2027/prep.py
@@ -262,5 +262,23 @@ def reverse_number(n):
         return int('-'+ strOutput)
     else:
         return int(strOutput)
-            
+
+            class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        result = []
+
+        for i in range(len(intervals)):
+            if newInterval[1] < intervals[i][0]:
+                result.append(newInterval)
+                return result + intervals[i:]
+            elif newInterval[0]> intervals[i][1]:
+                result.append(intervals[i])
+            else:
+                newInterval=[
+                    min(newInterval[0], intervals[i][0]),
+                    max(newInterval[1], intervals[i][1])
+                ]
+        result.append(newInterval)
+        return result
+        
         
